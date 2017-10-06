255914496946a670b057ac50d5e5b43dcc8e4842
Greg Fodor <gfodor@gmail.com>
2017-10-06 02:36:50 -0700

    Switch to local ip for host name

--- lib/ret/peerage_provider.ex
+++ lib/ret/peerage_provider.ex
@@ -27,7 +27,13 @@ defmodule Ret.PeerageProvider do
     fetch_json("http://#{habitat_ip}:#{habitat_port}/census")
     |> get_in(["census_groups", service_name, "population"])
     |> Map.values
-    |> Enum.map(&("ret@#{&1["sys"]["ip"]}"))
+    |> Enum.map(&("ret@#{hostname_to_local(&1["sys"]["hostname"])}"))
+    |> Enum.map(&(:erlang.binary_to_atom(&1, :utf8)))
+  end
+
+  defp hostname_to_local(hostname) do
+    [name | tail] = String.split(hostname, ".")
+    Enum.join(["#{name}-local" | tail], ".")
   end
 
   defp fetch_json(url) do
