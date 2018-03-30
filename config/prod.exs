f5f921bfbfdd3878833db229e74ff3900684e568
Greg Fodor <gfodor@gmail.com>
2018-03-30 16:42:59 -0700

    Fix for Endpoint url

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :ret, RetWeb.Endpoint,
   http: [],
-  url: [host: "reticulum.io", port: 80],
+  url: [host: "dev.reticulum.io", port: 80],
   static_url: [scheme: "https", host: "assets-dev.reticulum.io", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
