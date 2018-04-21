d2e4aaad8ab24ad61fb0a006116f0a64401cf9b0
Greg Fodor <gfodor@gmail.com>
2018-04-21 09:40:08 -0700

    Refactor

--- lib/ret_web/header_authorization.ex
+++ lib/ret_web/header_authorization.ex
@@ -4,11 +4,11 @@ defmodule RetWeb.Plugs.HeaderAuthorization do
   def init(default), do: default
 
   def call(conn, _default) do
-    header_name = Application.get_env(:ret, __MODULE__)[:header_name]
-    expected_header_value = Application.get_env(:ret, __MODULE__)[:header_value]
+    env = Application.get_env(:ret, __MODULE__)
+    [header_name, expected_value] = [env[:header_name], env[:header_value]]
 
     case conn |> get_req_header(header_name) do
-      [^expected_header_value] -> conn
+      [^expected_value] -> conn
       _ -> conn |> send_resp(401, "") |> halt()
     end
   end
