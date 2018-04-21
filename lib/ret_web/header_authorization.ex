321951553ac23d5ce2566330e04630c3bdc54076
Greg Fodor <gfodor@gmail.com>
2018-04-21 09:35:37 -0700

    Don't burn in header creds for deny API

--- lib/ret_web/header_authorization.ex
+++ lib/ret_web/header_authorization.ex
@@ -1,20 +1,15 @@
 defmodule RetWeb.Plugs.HeaderAuthorization do
   import Plug.Conn
 
-  @header_name Application.get_env(:ret, __MODULE__)[:header_name]
-  @header_value Application.get_env(:ret, __MODULE__)[:header_value]
-
   def init(default), do: default
 
   def call(conn, _default) do
-    call_with_header_value(conn, conn |> get_req_header(@header_name))
-  end
+    header_name = Application.get_env(:ret, __MODULE__)[:header_name]
+    expected_header_value = Application.get_env(:ret, __MODULE__)[:header_value]
 
-  defp call_with_header_value(conn, [@header_value]) do
-    conn
-  end
-
-  defp call_with_header_value(conn, _) do
-    conn |> send_resp(401, "") |> halt()
+    case conn |> get_req_header(header_name) do
+      [^expected_header_value] -> conn
+      _ -> conn |> send_resp(401, "") |> halt()
+    end
   end
 end
