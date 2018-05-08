27be3d7ba6d921845b136a9636abd6da20445f59
C. Van Wiemeersch <cvan@users.noreply.github.com>
2018-05-08 15:19:28 -0700

    whitelist `https://dpdp.webvr.rocks` in CSP

--- lib/ret_web/router.ex
+++ lib/ret_web/router.ex
@@ -23,7 +23,7 @@ defmodule RetWeb.Router do
           content_security_policy:
             "default-src 'none'; script-src 'self' #{asset_hosts} https://cdn.rawgit.com https://aframe.io 'unsafe-eval'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://cdn.aframe.io #{
               asset_hosts
-            }; style-src 'self' https://fonts.googleapis.com #{asset_hosts} 'unsafe-inline'; connect-src 'self' https://sentry.prod.mozaws.net #{
+            }; style-src 'self' https://fonts.googleapis.com #{asset_hosts} 'unsafe-inline'; connect-src 'self' https://sentry.prod.mozaws.net https://dpdb.webvr.rocks #{
               asset_hosts
             } #{websocket_hosts} https://cdn.aframe.io https://www.mozilla.org data:; img-src 'self' #{asset_hosts} https://cdn.aframe.io data: blob:; media-src 'self' #{
               asset_hosts
