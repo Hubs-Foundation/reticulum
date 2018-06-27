8580d018b83799c4e87cc2668d37d6d1c5b95ca2
Greg Fodor <gfodor@gmail.com>
2018-06-27 23:15:14 +0000

    Allow passing index, add extract return values

--- lib/ret_web/views/api/v1/media_view.ex
+++ lib/ret_web/views/api/v1/media_view.ex
@@ -1,9 +1,7 @@
 defmodule RetWeb.Api.V1.MediaView do
   use RetWeb, :view
 
-  def render("show.json", %{raw_image_url: raw_image_url}) do
-    %{
-      images: %{raw: raw_image_url}
-    }
+  def render("show.json", %{images: images}) do
+    %{images: images}
   end
 end
