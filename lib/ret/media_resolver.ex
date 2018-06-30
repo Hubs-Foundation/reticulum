ca9e2b77e9958fb860d01320f5f8377de1296a4b
Greg Fodor <gfodor@gmail.com>
2018-06-30 18:35:43 +0000

    Add more domains to whitelist

--- lib/ret/media_resolver.ex
+++ lib/ret/media_resolver.ex
@@ -1,7 +1,36 @@
 defmodule Ret.MediaResolver do
   use Retry
 
-  @ytdl_url_hosts ["www.youtube.com", "youtube.com"]
+  @ytdl_url_hosts [
+    "www.youtube.com",
+    "youtube.com",
+    "twitch.tv",
+    "www.twitch.tv",
+    "imgur.com",
+    "instagram.com",
+    "www.instagram.com",
+    "soundcloud.com",
+    "www.soundcloud.com",
+    "crunchyroll.com",
+    "www.crunchyroll.com",
+    "www.tumblr.com",
+    "tumblr.com",
+    "twitter.com",
+    "www.twitter.com",
+    "facebook.com",
+    "www.facebook.com",
+    "periscope.com",
+    "www.periscope.com",
+    "drive.google.com",
+    "gfycat.com",
+    "www.gfycat.com",
+    "flickr.com",
+    "www.flickr.com",
+    "dropbox.com",
+    "www.dropbox.com",
+    "cloudflare.com",
+    "www.cloudflare.com"
+  ]
 
   def resolve(url) when is_binary(url) do
     url |> URI.parse() |> resolve
