53205bf556ae2b2222beef9de3bebb54e6906147
Greg Fodor <gfodor@gmail.com>
2018-07-03 18:57:40 +0000

    Refactoring

--- lib/ret/media_resolver.ex
+++ lib/ret/media_resolver.ex
@@ -1,6 +1,9 @@
 defmodule Ret.MediaResolver do
   use Retry
 
+  @ytdl_valid_status_codes [200, 302, 500]
+  @success_status_codes [200]
+
   @ytdl_root_hosts [
     "youtube.com",
     "imgur.com",
@@ -23,37 +26,26 @@ defmodule Ret.MediaResolver do
     resolve(uri, root_host)
   end
 
-  def resolve(%URI{host: nil}, _) do
+  def resolve(%URI{host: nil}, _root_host) do
     {:commit, nil}
   end
 
   def resolve(%URI{} = uri, root_host) when root_host in @ytdl_root_hosts do
-    ytdl_host = Application.get_env(:ret, Ret.MediaResolver)[:ytdl_host]
+    with ytdl_host when is_binary(ytdl_host) <- resolver_config[:ytdl_host] do
+      ytdl_format = "best[protocol*=http]"
+      encoded_url = uri |> URI.to_string() |> URI.encode()
+      ytdl_url = "#{ytdl_host}/api/play?format=#{URI.encode(ytdl_format)}&url=#{encoded_url}"
+      ytdl_resp = retry_get_until_valid_ytdl_response(ytdl_url)
 
-    if ytdl_host do
-      with ytdl_format <- "best[protocol*=http]",
-           encoded_url <- uri |> URI.to_string() |> URI.encode(),
-           ytdl_url <-
-             "#{ytdl_host}/api/play?format=#{URI.encode(ytdl_format)}&url=#{encoded_url}" do
-        ytdl_resp =
-          retry with: exp_backoff() |> randomize |> cap(3_000) |> expiry(7_000) do
-            HTTPoison.get!(ytdl_url)
-          after
-            result -> result
-          else
-            error -> error
-          end
+      case ytdl_resp do
+        %HTTPoison.Response{status_code: 302, headers: headers} ->
+          {:commit, headers |> media_url_from_headers}
 
-        case ytdl_resp do
-          %HTTPoison.Response{status_code: 302, headers: headers} ->
-            {:commit, headers |> media_url_from_headers}
-
-          _ ->
-            resolve_non_video(uri, root_host)
-        end
+        _ ->
+          resolve_non_video(uri, root_host)
       end
     else
-      resolve_non_video(uri, root_host)
+      err -> resolve_non_video(uri, root_host)
     end
   end
 
@@ -63,29 +55,26 @@ defmodule Ret.MediaResolver do
 
   def resolve_non_video(%URI{} = uri, "deviantart.com") do
     uri =
-      case Application.get_env(:ret, Ret.MediaResolver)[:deviantart_client_id] do
-        client_id when is_binary(client_id) ->
-          page_resp = retry_until_200(uri |> URI.to_string())
+      with client_id when is_binary(client_id) <- resolver_config[:deviantart_client_id],
+           client_secret when is_binary(client_secret) <-
+             resolver_config[:deviantart_client_secret] do
+        page_resp = retry_get_until_success(uri |> URI.to_string())
+        deviant_id = Regex.run(@deviant_id_regex, page_resp.body) |> Enum.at(1)
+        token_host = "https://www.deviantart.com/oauth2/token"
+        api_host = "https://www.deviantart.com/api/v1/oauth2"
 
-          with deviant_id <- Regex.run(@deviant_id_regex, page_resp.body) |> Enum.at(1),
-               token_host <- "https://www.deviantart.com/oauth2/token",
-               api_host <- "https://www.deviantart.com/api/v1/oauth2",
-               client_secret <-
-                 Application.get_env(:ret, Ret.MediaResolver)[:deviantart_client_secret] do
-            token_url =
-              "#{token_host}?client_id=#{client_id}&client_secret=#{client_secret}&grant_type=client_credentials"
+        token_url =
+          "#{token_host}?client_id=#{client_id}&client_secret=#{client_secret}&grant_type=client_credentials"
 
-            token_resp = retry_until_200(token_url)
-            token = token_resp.body |> Poison.decode!() |> Kernel.get_in(["access_token"])
+        token_resp = retry_get_until_success(token_url)
+        token = token_resp.body |> Poison.decode!() |> Kernel.get_in(["access_token"])
 
-            api_url = "#{api_host}/deviation/#{deviant_id}?access_token=#{token}"
-            api_resp = retry_until_200(api_url)
+        api_url = "#{api_host}/deviation/#{deviant_id}?access_token=#{token}"
+        api_resp = retry_get_until_success(api_url)
 
-            api_resp.body |> Poison.decode!() |> Kernel.get_in(["content", "src"]) |> URI.parse()
-          end
-
-        _ ->
-          uri
+        api_resp.body |> Poison.decode!() |> Kernel.get_in(["content", "src"]) |> URI.parse()
+      else
+        err -> uri
       end
 
     {:commit, uri |> URI.to_string()}
@@ -93,40 +82,23 @@ defmodule Ret.MediaResolver do
 
   def resolve_non_video(%URI{} = uri, "giphy.com") do
     uri =
-      case Application.get_env(:ret, Ret.MediaResolver)[:giphy_api_key] do
-        giphy_api_key when is_binary(giphy_api_key) ->
-          with gif_id <-
-                 uri.path |> String.split("/") |> List.last() |> String.split("-") |> List.last(),
-               giphy_api_url <- "https://api.giphy.com/v1/gifs/#{gif_id}?api_key=#{giphy_api_key}" do
-            giphy_resp = retry_until_200(giphy_api_url)
+      with api_key when is_binary(api_key) <- resolver_config[:giphy_api_key] do
+        gif_id = uri.path |> String.split("/") |> List.last() |> String.split("-") |> List.last()
+        giphy_api_url = "https://api.giphy.com/v1/gifs/#{gif_id}?api_key=#{api_key}"
+        giphy_resp = retry_get_until_success(giphy_api_url)
 
-            original_image =
-              giphy_resp.body |> Poison.decode!() |> Kernel.get_in(["data", "images", "original"])
+        original_image =
+          giphy_resp.body |> Poison.decode!() |> Kernel.get_in(["data", "images", "original"])
 
-            (original_image["mp4"] || original_image["url"]) |> URI.parse()
-          end
-
-        _ ->
-          uri
+        (original_image["mp4"] || original_image["url"]) |> URI.parse()
+      else
+        err -> uri
       end
 
     {:commit, uri |> URI.to_string()}
   end
 
   def resolve_non_video(%URI{} = uri, "imgur.com") do
-    # imgur_client_id = Application.get_env(:ret, Ret.MediaResolver)[:imgur_client_id]
-
-    # if imgur_client_id != nil
-    #  ytdl_resp =
-    #    retry with: exp_backoff() |> randomize |> cap(3_000) |> expiry(15_000) do
-    #      HTTPoison.get!(ytdl_url)
-    #    after
-    #      result -> result
-    #    else
-    #      error -> error
-    #    end
-    # end
-
     {:commit, uri |> URI.to_string()}
   end
 
@@ -134,15 +106,38 @@ defmodule Ret.MediaResolver do
     {:commit, uri |> URI.to_string()}
   end
 
-  defp retry_until_200(url) do
+  defp retry_get_until_success(url) do
     retry with: exp_backoff() |> randomize |> cap(5_000) |> expiry(10_000) do
       case HTTPoison.get!(url) do
-        %{status_code: 200} = resp ->
+        %{status_code: status_code} = resp when status_code in @success_status_codes ->
           resp
 
         resp ->
-          IO.inspect(resp)
-          raise "API error for #{url}"
+          raise "Retry error for #{url} #{resp}"
+      end
+    after
+      result -> result
+    else
+      error -> error
+    end
+  end
+
+  # Performs a GET until we get response with a valid status code from ytdl.
+  #
+  # Oddly, valid status codes are 200, 302, and 500 since that indicates
+  # the server successfully attempted to resolve the video URL(s). If we get
+  # a different status code, this could indicate an outage or error in the
+  # request.
+  #
+  # https://youtube-dl-api-server.readthedocs.io/en/latest/api.html#api-methods
+  defp retry_get_until_valid_ytdl_response(url) do
+    retry with: exp_backoff() |> randomize |> cap(5_000) |> expiry(10_000) do
+      case HTTPoison.get!(url) do
+        %{status_code: status_code} = resp when status_code in @ytdl_valid_status_codes ->
+          resp
+
+        resp ->
+          raise "Retry error for #{url} #{resp}"
       end
     after
       result -> result
@@ -162,5 +157,9 @@ defmodule Ret.MediaResolver do
 
   defp media_url_from_headers(headers) do
     headers |> List.keyfind("Location", 0) |> elem(1)
+  end
+
+  defp resolver_config do
+    Application.get_env(:ret, Ret.MediaResolver)
   end
 end
