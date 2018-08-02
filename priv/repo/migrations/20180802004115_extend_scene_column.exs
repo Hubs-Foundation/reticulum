92083c089ccc91d716421f0ec0c0d2db7b13e215
Greg Fodor <gfodor@gmail.com>
2018-08-02 00:44:18 +0000

    Extend gltf scene column to 2048

---
+++ priv/repo/migrations/20180802004115_extend_scene_column.exs
@@ -0,0 +1,9 @@
+defmodule Ret.Repo.Migrations.ExtendSceneColumn do
+  use Ecto.Migration
+
+  def change do
+    alter table("hubs") do
+      modify(:default_environment_gltf_bundle_url, :string, null: false, size: 2048)
+    end
+  end
+end
