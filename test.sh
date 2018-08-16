0b118c60c4741002725e673ed72bee6b8d50eddc
Brian Peiris <brianpeiris@gmail.com>
2018-08-16 13:56:36 -0700

    revert dev https

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-result=$(curl -X POST -H 'content-type: application/json' localhost:4000/api/v1/scenes -d '
+result=$(curl -k -X POST -H 'content-type: application/json' https://localhost:4000/api/v1/scenes -d '
 {"scene": {
 	"name": "test",
 	"description": "a test scene",
@@ -8,6 +8,8 @@ result=$(curl -X POST -H 'content-type: application/json' localhost:4000/api/v1/
 	"upload_id": "234234"
 }}')
 echo "$result"
+
 scene_id=$(echo $result | jq -r '.scene_id')
 echo "$scene_id"
-curl "localhost:4000/api/v1/scenes/$scene_id"
+
+curl -k "https://localhost:4000/api/v1/scenes/$scene_id"
