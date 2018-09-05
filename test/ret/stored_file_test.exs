b5064ed96deb4d498457e69ce25a8e7cf52dae89
Greg Fodor <gfodor@gmail.com>
2018-09-05 16:43:01 +0000

    Unit test for promote_multi

--- test/ret/stored_file_test.exs
+++ test/ret/stored_file_test.exs
@@ -10,7 +10,7 @@ defmodule Ret.StoredFileTest do
   end
 
   setup _context do
-    %{temp_file: generate_temp_file()}
+    %{temp_file: generate_temp_file(), temp_file_2: generate_temp_file()}
   end
 
   test "store a file", %{temp_file: temp_file} do
@@ -34,10 +34,9 @@ defmodule Ret.StoredFileTest do
 
     {:ok, uuid} = StoredFiles.store(%Plug.Upload{path: temp_file}, "text/plain", "secret")
     {:ok, stored_file} = StoredFiles.promote(uuid, "secret", account)
-    {:ok, %{"content_type" => content_type}, stream} = StoredFiles.fetch(stored_file)
+    result = StoredFiles.fetch(stored_file)
 
-    assert content_type == "text/plain"
-    assert stream |> Enum.map(& &1) |> Enum.join() == "test"
+    assert_fetch_result(result, "text/plain", "test")
   end
 
   test "should be able to re-promote without failure", %{temp_file: temp_file} do
@@ -50,6 +49,32 @@ defmodule Ret.StoredFileTest do
 
     {:ok, %StoredFile{stored_file_id: ^stored_file_id}} =
       StoredFiles.promote(uuid, "secret", account)
+  end
+
+  test "should be able to promote multiple files", %{
+    temp_file: temp_file,
+    temp_file_2: temp_file_2
+  } do
+    account = Ret.Repo.insert!(%Ret.Account{})
+
+    {:ok, uuid_1} = StoredFiles.store(%Plug.Upload{path: temp_file}, "text/plain", "secret")
+    {:ok, uuid_2} = StoredFiles.store(%Plug.Upload{path: temp_file_2}, "text/plain", "secret")
+
+    {:ok, %{t1: stored_file_t1, t2: stored_file_t2}} =
+      StoredFiles.promote_multi(%{t1: {uuid_1, "secret"}, t2: {uuid_2, "secret"}}, account)
+
+    r1 = StoredFiles.fetch(stored_file_t1)
+    r2 = StoredFiles.fetch(stored_file_t2)
+
+    assert_fetch_result(r1, "text/plain", "test")
+    assert_fetch_result(r2, "text/plain", "test")
+  end
+
+  defp assert_fetch_result(result, expected_content_type, expected_content) do
+    {:ok, %{"content_type" => content_type}, stream} = result
+
+    assert content_type == expected_content_type
+    assert stream |> Enum.map(& &1) |> Enum.join() == expected_content
   end
 
   defp generate_temp_file do
