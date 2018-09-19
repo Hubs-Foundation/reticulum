2fd32e522d0831b7b7f78d038e915d454302fad8
Brian Peiris <brianpeiris@gmail.com>
2018-09-18 19:15:09 -0700

    Add attribution to scene

--- lib/ret_web/views/api/v1/scene_view.ex
+++ lib/ret_web/views/api/v1/scene_view.ex
@@ -21,6 +21,7 @@ defmodule RetWeb.Api.V1.SceneView do
           scene_id: scene.scene_sid,
           name: scene.name,
           description: scene.description,
+          attribution: scene.attribution,
           model_url: scene.model_owned_file |> OwnedFile.uri_for() |> URI.to_string(),
           screenshot_url: scene.screenshot_owned_file |> OwnedFile.uri_for() |> URI.to_string(),
           url: url_for_scene(scene)
