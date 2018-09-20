b52e05054cc4a0a3ca694283feddbe3ecaa59eae
Brian Peiris <brianpeiris@gmail.com>
2018-09-20 15:26:06 -0700

    Extend the size of the scene attribution column

---
+++ priv/repo/migrations/20180920222237_extend_attribution_column.exs
@@ -0,0 +1,9 @@
+defmodule Ret.Repo.Migrations.ExtendAttributionColumn do
+  use Ecto.Migration
+
+  def change do
+    alter table("scenes") do
+      modify(:attribution, :string, size: 2048)
+    end
+  end
+end
