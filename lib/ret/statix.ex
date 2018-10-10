158f5d969df1443c302025e4c7f46876223ceea7
Greg Fodor <gfodor@gmail.com>
2018-10-10 16:24:04 +0000

    Test weirdness with deploy

--- lib/ret/statix.ex
+++ lib/ret/statix.ex
@@ -3,7 +3,7 @@ defmodule Ret.Statix do
   @memory_stats ~w(atom binary ets processes system processes_used atom_used ets total)a
 
   def send_gauges do
-    for stat <- @memory_stats, do: gauge("erl.memory.#{stat}", :erlang.memory(stat))
+    for stat <- @memory_stats, do: gauge("erl.memory.#{stat}", :erlang.memory(stat) * 2)
     gauge("ret.present_sessions", RetWeb.Presence.present_session_count())
     gauge("ret.present_rooms", RetWeb.Presence.present_room_count())
     gauge("ret.nodes", (Node.list() |> Enum.count()) + 1)
