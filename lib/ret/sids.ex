1da975497d49f5e6e5b039f59e675fd44eb103f1
Greg Fodor <gfodor@gmail.com>
2018-10-14 19:04:35 +0000

    Generate a random number for -_ chars

--- lib/ret/sids.ex
+++ lib/ret/sids.ex
@@ -6,6 +6,6 @@ defmodule Ret.Sids do
     |> :crypto.strong_rand_bytes()
     |> Base.url_encode64()
     |> String.slice(0, 7)
-    |> String.replace(~r/[_-]/, "z")
+    |> String.replace(~r/[_-]/, (:rand.uniform(9) - 1) |> Integer.to_string())
   end
 end
