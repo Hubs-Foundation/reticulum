b050111450b5896f3314f5f654b182f8b03bfad8
Brian Peiris <brianpeiris@gmail.com>
2018-11-27 16:07:08 -0800

    Migrate existing room objects to use a default account record. Make account_id non-null.

--- priv/repo/migrations/20181113033726_add_account_to_room_objects.exs
+++ priv/repo/migrations/20181113033726_add_account_to_room_objects.exs
@@ -2,8 +2,17 @@ defmodule Ret.Repo.Migrations.AddAccountToRoomObjects do
   use Ecto.Migration
 
   def change do
+    # Add default account record for existing room objects.
+    %Ret.Account{account_id: 0} |> Ret.Repo.insert()
+
     alter table("room_objects") do
-      add(:account_id, references(:accounts, column: :account_id))
+      add(:account_id, references(:accounts, column: :account_id), default: 0)
+    end
+
+    flush()
+
+    alter table("room_objects") do
+      modify(:account_id, :bigint, default: nil, null: false)
     end
   end
 end
