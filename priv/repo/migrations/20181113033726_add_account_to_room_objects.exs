384fc79bd5f908a16949a75e14c510e67eed8f31
Brian Peiris <brianpeiris@gmail.com>
2018-11-27 17:08:23 -0800

    Use check constraint instead of dummy account

--- priv/repo/migrations/20181113033726_add_account_to_room_objects.exs
+++ priv/repo/migrations/20181113033726_add_account_to_room_objects.exs
@@ -2,17 +2,16 @@ defmodule Ret.Repo.Migrations.AddAccountToRoomObjects do
   use Ecto.Migration
 
   def change do
-    # Add default account record for existing room objects.
-    %Ret.Account{account_id: 0} |> Ret.Repo.insert()
-
     alter table("room_objects") do
-      add(:account_id, references(:accounts, column: :account_id), default: 0)
+      add(:account_id, references(:accounts, column: :account_id))
     end
 
-    flush()
-
-    alter table("room_objects") do
-      modify(:account_id, :bigint, default: nil, null: false)
-    end
+    create(
+      constraint(
+        "room_objects",
+        "room_object_is_legacy_or_has_account",
+        check: "inserted_at < '#{Ecto.DateTime.utc() |> Ecto.DateTime.to_iso8601()}' or account_id is not null"
+      )
+    )
   end
 end
