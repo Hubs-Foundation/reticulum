9083afb56b68696c743a3360d35c22c7e0ef5472
Greg Fodor <gfodor@gmail.com>
2018-12-27 23:07:21 +0000

    Use census for full service name discovery

--- lib/ret/habitat.ex
+++ lib/ret/habitat.ex
@@ -4,33 +4,26 @@ defmodule Ret.Habitat do
   end
 
   def get_hosts_for_service(service_name, hostname_xform \\ fn x -> x end) do
-    full_service_name = service_name |> get_full_service_name
     habitat_config = Application.get_env(:ret, Ret.Habitat)
     habitat_ip = habitat_config[:ip]
     habitat_port = habitat_config[:http_port]
+    census_url = "http://#{habitat_ip}:#{habitat_port}/census"
 
-    fetch_json("http://#{habitat_ip}:#{habitat_port}/census")
+    %{status_code: 200, body: census_body} = HTTPoison.get!(url)
+    census_json = census_body |> Poison.decode!()
+
+    full_service_name =
+      census_json
+      |> get_in(["census_groups"])
+      |> Map.keys()
+      |> Enum.filter(&String.starts_with?(&1, "#{service_name}."))
+      |> List.first()
+
+    census_json
     |> get_in(["census_groups", full_service_name, "population"])
     |> Map.values()
     |> Enum.filter(&(&1["alive"] == true))
     |> Enum.map(&hostname_xform.(&1["sys"]["hostname"]))
     |> Enum.map(&:erlang.binary_to_atom(&1, :utf8))
-  end
-
-  defp get_full_service_name(service_name) do
-    habitat_config = Application.get_env(:ret, Ret.Habitat)
-    habitat_ip = habitat_config[:ip]
-    habitat_port = habitat_config[:http_port]
-
-    "http://#{habitat_ip}:#{habitat_port}/services"
-    |> fetch_json
-    |> Enum.map(& &1["service_group"])
-    |> Enum.filter(&String.starts_with?(&1, service_name))
-    |> List.first()
-  end
-
-  defp fetch_json(url) do
-    %{status_code: 200, body: body} = HTTPoison.get!(url)
-    body |> Poison.decode!()
   end
 end
