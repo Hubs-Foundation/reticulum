cfce8106a32cf1435c159f2b8627d75c9e8107cf
Greg Fodor <gfodor@gmail.com>
2019-01-02 23:02:25 +0000

    Move scene info into hubs view

--- lib/ret_web/views/api/v1/scene_view.ex
+++ lib/ret_web/views/api/v1/scene_view.ex
@@ -7,27 +7,23 @@ defmodule RetWeb.Api.V1.SceneView do
   end
 
   def render("create.json", %{scene: scene}) do
-    render_scene(scene)
+    %{scenes: [render_scene(scene)]}
   end
 
   def render("show.json", %{scene: scene}) do
-    render_scene(scene)
+    %{scenes: [render_scene(scene)]}
   end
 
-  defp render_scene(scene) do
+  def render_scene(scene) do
     %{
-      scenes: [
-        %{
-          scene_id: scene.scene_sid,
-          name: scene.name,
-          description: scene.description,
-          attribution: scene.attribution,
-          attributions: scene.attributions,
-          model_url: scene.model_owned_file |> OwnedFile.uri_for() |> URI.to_string(),
-          screenshot_url: scene.screenshot_owned_file |> OwnedFile.uri_for() |> URI.to_string(),
-          url: url_for_scene(scene)
-        }
-      ]
+      scene_id: scene.scene_sid,
+      name: scene.name,
+      description: scene.description,
+      attribution: scene.attribution,
+      attributions: scene.attributions,
+      model_url: scene.model_owned_file |> OwnedFile.uri_for() |> URI.to_string(),
+      screenshot_url: scene.screenshot_owned_file |> OwnedFile.uri_for() |> URI.to_string(),
+      url: url_for_scene(scene)
     }
   end
 end
