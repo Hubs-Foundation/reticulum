c0a62788008298b452cf3ab978415f606e2dcedd
Robert Long <robert@robertlong.me>
2019-03-08 15:56:50 -0800

    Require created_by_account_id

--- priv/repo/migrations/20190306032224_create_projects.exs
+++ priv/repo/migrations/20190306032224_create_projects.exs
@@ -6,7 +6,7 @@ defmodule Ret.Repo.Migrations.CreateProjects do
       add(:project_id, :bigint, default: fragment("ret0.next_id()"), primary_key: true)
       add(:project_sid, :string)
       add(:name, :string, null: false)
-      add(:created_by_account_id, references(:accounts, column: :account_id))
+      add(:created_by_account_id, references(:accounts, column: :account_id), null: false)
       add(:project_owned_file_id, :bigint, null: true)
       add(:thumbnail_owned_file_id, :bigint, null: true)
 
