2ef9c8956bf31f104ef2dd860098d1cb99afef17
Brian Peiris <brianpeiris@gmail.com>
2019-03-27 11:07:01 -0700

    Fix hub_binding create

--- lib/ret_web/controllers/api/v1/hub_binding_controller.ex
+++ lib/ret_web/controllers/api/v1/hub_binding_controller.ex
@@ -8,9 +8,10 @@ defmodule RetWeb.Api.V1.HubBindingController do
         %{"hub_binding" => %{"hub_id" => _, "type" => _, "community_id" => community_id, "channel_id" => channel_id}} =
           params
       ) do
+    hub_binding = HubBinding |> Repo.get_by(community_id: community_id, channel_id: channel_id)
+
     {result, _} =
-      HubBinding
-      |> Repo.get_by(community_id: community_id, channel_id: channel_id)
+      (hub_binding || %HubBinding{})
       |> HubBinding.changeset(params["hub_binding"])
       |> Repo.insert_or_update()
 
