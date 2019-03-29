c0a9aa0930094b1fd79d92de212191bad9d40cbb
Brian Peiris <brianpeiris@gmail.com>
2019-03-29 11:21:03 -0700

    refactor

--- lib/ret_web/controllers/api/v1/oauth_controller.ex
+++ lib/ret_web/controllers/api/v1/oauth_controller.ex
@@ -1,35 +1,37 @@
 defmodule RetWeb.Api.V1.OAuthController do
   use RetWeb, :controller
 
+  alias Ret.{Repo, OAuthToken, OAuthProvider, DiscordClient, Hub, Account, PermsToken}
+
   plug(RetWeb.Plugs.RateLimit when action in [:show])
 
   def show(conn, %{"type" => "discord", "state" => state, "code" => code}) do
-    {:ok, %{"hub_sid" => hub_sid}} = Ret.OAuthToken.decode_and_verify(state)
+    {:ok, %{"hub_sid" => hub_sid}} = OAuthToken.decode_and_verify(state)
 
     %{"id" => discord_user_id, "email" => email, "verified" => verified} =
-      code |> Ret.DiscordClient.fetch_access_token() |> Ret.DiscordClient.fetch_user_info()
+      code |> DiscordClient.fetch_access_token() |> DiscordClient.fetch_user_info()
 
-    hub = Ret.Hub |> Ret.Repo.get_by(hub_sid: hub_sid) |> Ret.Repo.preload(:hub_bindings)
+    hub = Hub |> Repo.get_by(hub_sid: hub_sid) |> Repo.preload(:hub_bindings)
 
     conn
     |> process_oauth(discord_user_id, verified, email, hub)
-    |> put_resp_header("location", hub |> Ret.Hub.url_for())
+    |> put_resp_header("location", hub |> Hub.url_for())
     |> send_resp(307, "")
   end
 
   # Discord user has a verified email, so we create a Hubs account for them associate it with their discord user id.
   defp process_oauth(conn, discord_user_id, true = _verified, email, _hub) do
-    account = email |> Ret.Account.account_for_email()
+    account = email |> Account.account_for_email()
 
-    oauth_provider = Ret.OAuthProvider |> Ret.Repo.get_by(source: :discord, account_id: account.account_id)
+    oauth_provider = OAuthProvider |> Repo.get_by(source: :discord, account_id: account.account_id)
 
-    (oauth_provider || %Ret.OAuthProvider{source: :discord, account: account})
+    (oauth_provider || %OAuthProvider{source: :discord, account: account})
     |> Ecto.Changeset.change(provider_account_id: discord_user_id)
-    |> Ret.Repo.insert_or_update()
+    |> Repo.insert_or_update()
 
     credentials = %{
       email: email,
-      token: email |> Ret.Account.account_for_email() |> Ret.Account.credentials_for_account()
+      token: email |> Account.account_for_email() |> Account.credentials_for_account()
     }
 
     conn |> put_short_lived_cookie("ret-oauth-flow-account-credentials", credentials |> Poison.encode!())
@@ -40,14 +42,14 @@ defmodule RetWeb.Api.V1.OAuthController do
   defp process_oauth(conn, discord_user_id, false = _verified, _email, hub) do
     hub_binding = hub.hub_bindings |> Enum.find(&(&1.type == :discord))
 
-    can_join_hub = discord_user_id |> Ret.DiscordClient.member_of_channel?(hub_binding)
+    can_join_hub = discord_user_id |> DiscordClient.member_of_channel?(hub_binding)
 
     perms_token =
       %{
         join_hub: can_join_hub,
         kick_users: false
       }
-      |> Ret.PermsToken.token_for_perms()
+      |> PermsToken.token_for_perms()
 
     conn |> put_short_lived_cookie("ret-oauth-flow-perms-token", perms_token)
   end
