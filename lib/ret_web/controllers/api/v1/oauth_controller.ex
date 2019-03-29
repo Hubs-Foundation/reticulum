5bfa31c0a2ec595e02845d75e3367496ebcbe978
Brian Peiris <brianpeiris@gmail.com>
2019-03-28 19:23:16 -0700

    Refactor oauth controller

--- lib/ret_web/controllers/api/v1/oauth_controller.ex
+++ lib/ret_web/controllers/api/v1/oauth_controller.ex
@@ -4,52 +4,53 @@ defmodule RetWeb.Api.V1.OAuthController do
   plug(RetWeb.Plugs.RateLimit when action in [:show])
 
   def show(conn, %{"type" => "discord", "state" => state, "code" => code}) do
-    {:ok, claims} = Ret.OAuthToken.decode_and_verify(state)
-    %{"hub_sid" => hub_sid} = claims
+    {:ok, %{"hub_sid" => hub_sid}} = Ret.OAuthToken.decode_and_verify(state)
 
     %{"id" => discord_user_id, "email" => email, "verified" => verified} =
       code |> Ret.DiscordClient.fetch_access_token() |> Ret.DiscordClient.fetch_user_info()
 
     hub = Ret.Hub |> Ret.Repo.get_by(hub_sid: hub_sid) |> Ret.Repo.preload(:hub_bindings)
-    url = hub |> Ret.Hub.url_for()
 
-    if verified do
-      account = email |> Ret.Account.account_for_email()
+    conn
+    |> process_oauth(discord_user_id, email, verified, hub)
+    |> put_resp_header("location", hub |> Ret.Hub.url_for())
+    |> send_resp(307, "")
+  end
 
-      oauth_provider = Ret.OAuthProvider |> Ret.Repo.get_by(source: :discord, account_id: account.account_id)
+  # Discord user has a verified email, so we create a Hubs account for them associate it with their discord user id.
+  defp process_oauth(conn, discord_user_id, email, true = _verified, _hub) do
+    account = email |> Ret.Account.account_for_email()
 
-      (oauth_provider || %Ret.OAuthProvider{source: :discord, account: account})
-      |> Ecto.Changeset.change(provider_account_id: discord_user_id)
-      |> Ret.Repo.insert_or_update()
+    oauth_provider = Ret.OAuthProvider |> Ret.Repo.get_by(source: :discord, account_id: account.account_id)
 
-      credentials = %{
-        email: email,
-        token: email |> Ret.Account.account_for_email() |> Ret.Account.credentials_for_account()
+    (oauth_provider || %Ret.OAuthProvider{source: :discord, account: account})
+    |> Ecto.Changeset.change(provider_account_id: discord_user_id)
+    |> Ret.Repo.insert_or_update()
+
+    credentials = %{
+      email: email,
+      token: email |> Ret.Account.account_for_email() |> Ret.Account.credentials_for_account()
+    }
+
+    conn |> put_short_lived_cookie("ret-oauth-flow-account-credentials", credentials |> Poison.encode!())
+  end
+
+  # Discord user does not have a verified email, so we can't create an account for them. Instead, we generate a perms
+  # token to let them join the hub if permitted.
+  defp process_oauth(conn, discord_user_id, email, false = _verified, hub) do
+    hub_binding = hub.hub_bindings |> Enum.find(&(&1.type == :discord))
+
+    can_join_hub =
+      discord_user_id |> Ret.DiscordClient.member_of_channel?(hub_binding.community_id, hub_binding.channel_id)
+
+    perms_token =
+      %{
+        join_hub: can_join_hub,
+        kick_users: false
       }
+      |> Ret.PermsToken.token_for_perms()
 
-      conn
-      |> put_short_lived_cookie("ret-oauth-flow-account-credentials", credentials |> Poison.encode!())
-      |> put_resp_header("location", url)
-      |> send_resp(307, "")
-    else
-      hub_binding = hub.hub_bindings |> Enum.find(&(&1.type == :discord))
-
-      perms_token =
-        %{
-          join_hub:
-            discord_user_id
-            |> Ret.DiscordClient.member_of_channel?(hub_binding.community_id, hub_binding.channel_id),
-          kick_users: false
-        }
-        |> Ret.PermsToken.token_for_perms()
-
-      conn
-      |> put_short_lived_cookie("ret-oauth-flow-perms-token", perms_token)
-      |> put_resp_header("location", url)
-      |> send_resp(307, "")
-    end
-
-    conn |> send_resp(307, "")
+    conn |> put_short_lived_cookie("ret-oauth-flow-perms-token", perms_token)
   end
 
   defp put_short_lived_cookie(conn, key, value) do
