b46c9823016f2a2b43c1af4b6fa8b2847b7a3ddd
Greg Fodor <gfodor@gmail.com>
2019-05-01 00:54:06 +0000

    Remove rate limiting on meta controller because it breaks deploys when multiple tabs open

--- lib/ret_web/controllers/meta_controller.ex
+++ lib/ret_web/controllers/meta_controller.ex
@@ -1,8 +1,6 @@
 defmodule RetWeb.Api.V1.MetaController do
   use RetWeb, :controller
 
-  plug(RetWeb.Plugs.RateLimit when action in [:show])
-
   def show(conn, _params) do
     meta = Ret.Meta.get_meta()
     conn |> send_resp(200, meta |> Poison.encode!())
