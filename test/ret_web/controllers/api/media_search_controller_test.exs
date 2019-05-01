ff595959a6e2ada45259df5f607686089a0adce0
netpro2k <netpro2k@gmail.com>
2019-04-30 18:22:27 -0700

    Expand tests a bit and dry them up

--- test/ret_web/controllers/api/media_search_controller_test.exs
+++ test/ret_web/controllers/api/media_search_controller_test.exs
@@ -2,7 +2,7 @@ defmodule RetWeb.SceneControllerTest do
   use RetWeb.ConnCase
   import Ret.TestHelpers
 
-  setup [:create_account, :create_owned_file, :create_avatar]
+  alias Ret.Account
 
   setup do
     on_exit(fn ->
@@ -10,34 +10,74 @@ defmodule RetWeb.SceneControllerTest do
     end)
   end
 
-  @tag :authenticated
-  test "Search for avatar by user works for a user's own avatars", %{conn: conn, account: account, avatar: avatar} do
+  setup _context do
+    account_1 = Account.account_for_email("test@mozilla.com")
+    account_2 = Account.account_for_email("test2@mozilla.com")
+    account_3 = Account.account_for_email("test3@mozilla.com")
+
+    %{
+      account_1: account_1,
+      account_2: account_2,
+      account_3: account_3,
+      avatar_1: create_avatar(account_1),
+      avatar_2: create_avatar(account_2)
+    }
+  end
+
+  defp search_avatars_for_account_id(conn, account_id) do
+    conn
+    |> get(api_v1_media_search_path(conn, :index), %{
+      source: "avatars",
+      user: account_id |> Integer.to_string()
+    })
+  end
+
+  test "Search for a user's own avatars should return results if they have avatars", %{
+    conn: conn,
+    account_1: account,
+    avatar_1: avatar
+  } do
     resp =
       conn
+      |> auth_with_account(account)
+      |> search_avatars_for_account_id(avatar.account_id)
+      |> json_response(200)
+
+    # there should only be one entry
+    [entry] = resp["entries"]
+
+    # and the avatar should beelong to the account we authed as
+    assert entry["id"] == avatar.avatar_sid
+    assert avatar.account_id == account.account_id
+  end
+
+  test "Search for a user's own avatars should return an empty list if they have no avatars", %{
+    conn: conn,
+    account_3: account
+  } do
+    resp =
+      conn
+      |> auth_with_account(account)
       |> get(api_v1_media_search_path(conn, :index), %{
         source: "avatars",
         user: account.account_id |> Integer.to_string()
       })
       |> json_response(200)
 
-    # there should only be one entry
-    [entry] = resp["entries"]
-
-    # and it should be the avatar we just created
-    assert entry["id"] == avatar.avatar_sid
+    # There should be no entries
+    assert resp["entries"] == []
   end
 
-  test "Search for avatar by user does not work another user's avatars", %{conn: conn, account: account} do
-    {:ok, token, _claims} =
-      "test2@mozilla.com"
-      |> Ret.Account.account_for_email()
-      |> Ret.Guardian.encode_and_sign()
-
+  test "Search for another user's avatars should return a 401", %{
+    conn: conn,
+    avatar_1: avatar_1,
+    account_2: account
+  } do
     conn
-    |> Plug.Conn.put_req_header("authorization", "bearer: " <> token)
+    |> auth_with_account(account)
     |> get(api_v1_media_search_path(conn, :index), %{
       source: "avatars",
-      user: account.account_id |> Integer.to_string()
+      user: avatar_1.account_id |> Integer.to_string()
     })
     |> response(401)
   end
