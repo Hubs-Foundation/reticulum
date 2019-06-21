712ea9e53caf790ad701b1a88881d429dbd8198c
Greg Fodor <gfodor@gmail.com>
2019-06-21 22:43:32 +0000

    Stub out uploading

--- lib/ret/twitter_client.ex
+++ lib/ret/twitter_client.ex
@@ -2,6 +2,9 @@ defmodule Ret.TwitterClient do
   import Ret.HttpUtils
 
   @twitter_api_base "https://api.twitter.com"
+  @twitter_upload_api_base "https://upload.twitter.com"
+
+  alias Ret.{Storage}
 
   def get_oauth_url(hub_sid, account_id) do
     creds =
@@ -14,7 +17,7 @@ defmodule Ret.TwitterClient do
 
     token = Ret.OAuthToken.token_for_hub_and_account(hub_sid, account_id)
     callback_url = "#{get_redirect_uri()}?state=#{token}"
-    res = post("/oauth/request_token", [{"oauth_callback", callback_url}], creds)
+    res = post("#{@twitter_api_base}/oauth/request_token", [{"oauth_callback", callback_url}], creds)
     "#{@twitter_api_base}/oauth/authorize?" <> URI.encode_query(%{oauth_token: res["oauth_token"]})
   end
 
@@ -26,24 +29,44 @@ defmodule Ret.TwitterClient do
         token: request_token
       )
 
-    post("/oauth/access_token", [{"oauth_verifier", oauth_verifier}], creds)
+    post("#{@twitter_api_base}/oauth/access_token", [{"oauth_verifier", oauth_verifier}], creds)
   end
 
-  defp get(path, params, creds) do
-    url = "#{@twitter_api_base}#{path}"
+  def upload_stored_file_as_media(stored_file_id, stored_file_access_token, token, token_secret) do
+    url = "#{@twitter_upload_api_base}/1.1/media/upload.json"
 
-    oauther_params = OAuther.sign("get", url, params, creds)
-    encoded_params = URI.encode_query(oauther_params)
-    request_url = "#{url}?#{encoded_params}"
+    creds =
+      OAuther.credentials(
+        consumer_key: module_config(:consumer_key),
+        consumer_secret: module_config(:consumer_secret),
+        token: token,
+        token_secret: token_secret
+      )
 
-    retry_get_until_success(request_url)
-    |> Map.get(:body)
-    |> Poison.decode!()
+    case Storage.fetch(stored_file_id, stored_file_access_token) do
+      {:ok, %{"content_length" => total_bytes, "content_type" => media_type}, stream} ->
+        media_init_res =
+          post(
+            url,
+            [{"command", "INIT"}, {"total_bytes", total_bytes}, {"media_type", media_type}],
+            creds
+          )
+
+        case media_init_res do
+          %{"media_id" => media_id} ->
+            upload_media_chunks(creds, stream, media_id)
+            post(url, [{"command", "FINALIZE"}, {"media_id", media_id}], creds)
+
+          _ ->
+            nil
+        end
+
+      _ ->
+        nil
+    end
   end
 
-  defp post(path, params, creds) do
-    url = "#{@twitter_api_base}/#{path}"
-
+  defp post(url, params, creds) do
     params = OAuther.sign("post", url, params, creds)
     encoded_params = URI.encode_query(params)
 
@@ -53,7 +76,35 @@ defmodule Ret.TwitterClient do
     |> URI.decode_query()
   end
 
+  defp upload_media_chunks(creds, stream, media_id) do
+    Enum.reduce(stream, 0, fn chunk, chunk_idx ->
+      post(
+        "#{@twitter_upload_api_base}/1.1/media/upload.json",
+        [
+          {"command", "APPEND"},
+          {"media_id", media_id},
+          {"media_data", Base.encode64(chunk)},
+          {"segment_index", chunk_idx}
+        ],
+        creds
+      )
+
+      chunk_idx + 1
+    end)
+  end
+
   defp get_redirect_uri(), do: RetWeb.Endpoint.url() <> "/api/v1/oauth/twitter"
+
+  # Unused for now
+  # defp get(url, params, creds) do
+  #   oauther_params = OAuther.sign("get", url, params, creds)
+  #   encoded_params = URI.encode_query(oauther_params)
+  #   request_url = "#{url}?#{encoded_params}"
+
+  #   retry_get_until_success(request_url)
+  #   |> Map.get(:body)
+  #   |> Poison.decode!()
+  # end
 
   defp module_config(key) do
     Application.get_env(:ret, __MODULE__)[key]
--- lib/ret_web/controllers/twitter_controller.ex
+++ lib/ret_web/controllers/twitter_controller.ex
@@ -3,17 +3,34 @@
 # of interface
 defmodule RetWeb.Api.V1.TwitterController do
   use RetWeb, :controller
-  alias Ret.{TwitterClient, Account}
+  alias Ret.{TwitterClient, Storage, Account}
 
-  def tweets(conn, params) do
+  def tweets(conn, %{"media_stored_file_url" => media_stored_file_url, "body" => body}) do
     account = Guardian.Plug.current_resource(conn)
     oauth_provider = Account.oauth_provider_for_source(account, :twitter)
     token = oauth_provider.provider_access_token
     token_secret = oauth_provider.provider_access_token_secret
-    # result = TwitterClient.tweet(params["tweet"], token, token_secret)
-    # IO.inspect(result)
 
-    # conn |> send_resp(200, result |> Poison.encode!())
-    conn
+    case media_stored_file_url |> URI.parse() do
+      %URI{path: "/files/" <> filename, query: query} ->
+        [stored_file_id, _ext] = filename |> String.split(".")
+        parsed_query = query |> URI.decode_query()
+        stored_file_access_token = parsed_query["token"]
+
+        case TwitterClient.upload_stored_file_as_media(stored_file_id, stored_file_access_token, token, token_secret) do
+          media_id when is_binary(media_id) ->
+            media_id
+
+          _ ->
+            conn |> send_resp(400, "Invalid media stored file url")
+        end
+
+      # result = TwitterClient.tweet(params["tweet"], token, token_secret)
+      # IO.inspect(result)
+
+      # conn |> send_resp(200, result |> Poison.encode!())
+      _ ->
+        conn |> send_resp(400, "Invalid media stored file url")
+    end
   end
 end
