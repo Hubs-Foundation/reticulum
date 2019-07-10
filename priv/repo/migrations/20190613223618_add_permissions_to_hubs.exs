17dc040e676718151dfea5eb7010bc7b3b8856f1
Brian Peiris <brianpeiris@gmail.com>
2019-07-10 13:23:24 -0700

    Spell out default member permissions in migration

--- priv/repo/migrations/20190613223618_add_permissions_to_hubs.exs
+++ priv/repo/migrations/20190613223618_add_permissions_to_hubs.exs
@@ -4,7 +4,16 @@ defmodule Ret.Repo.Migrations.AddPermissionsToHubs do
 
   def change do
     alter table("hubs") do
-      add(:member_permissions, :integer, default: (1 <<< 4) - 1)
+      add(:member_permissions, :integer,
+        default:
+          %{
+            spawn_and_move_media: true,
+            spawn_camera: true,
+            spawn_drawing: true,
+            pin_objects: true
+          }
+          |> Ret.Hub.member_permissions_to_int()
+      )
     end
   end
 end
