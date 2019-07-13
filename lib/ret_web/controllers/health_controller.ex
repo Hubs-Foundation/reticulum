b4d0c7e7718afb907ef22d8424159baeebfbbc0e
Greg Fodor <gfodor@gmail.com>
2019-07-13 21:48:25 +0000

    Fix health check

--- lib/ret_web/controllers/health_controller.ex
+++ lib/ret_web/controllers/health_controller.ex
@@ -8,12 +8,12 @@ defmodule RetWeb.HealthController do
     from(h in Ret.Hub, limit: 1) |> Ret.Repo.one!()
 
     # Check page cache
-    assert Ret.PageOriginWarmer.chunks_for_page(:hubs, "index.html") |> elem(1) |> Enum.count() > 0
-    assert Ret.PageOriginWarmer.chunks_for_page(:hubs, "hub.html") |> elem(1) |> Enum.count() > 0
-    assert Ret.PageOriginWarmer.chunks_for_page(:spoke, "index.html") |> elem(1) |> Enum.count() > 0
+    true = Ret.PageOriginWarmer.chunks_for_page(:hubs, "index.html") |> elem(1) |> Enum.count() > 0
+    true = Ret.PageOriginWarmer.chunks_for_page(:hubs, "hub.html") |> elem(1) |> Enum.count() > 0
+    true = Ret.PageOriginWarmer.chunks_for_page(:spoke, "index.html") |> elem(1) |> Enum.count() > 0
 
     # Check room routing
-    assert Ret.RoomAssigner.get_available_host("") != nil
+    true = Ret.RoomAssigner.get_available_host("") != nil
 
     send_resp(conn, 200, "ok")
   end
