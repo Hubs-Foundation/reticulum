d76164bf9079858cfd0498145177a48e7cfd67f5
Greg Fodor <gfodor@gmail.com>
2019-07-19 16:01:48 +0000

    Look up pages in cachex for health check

--- lib/ret_web/controllers/health_controller.ex
+++ lib/ret_web/controllers/health_controller.ex
@@ -7,9 +7,9 @@ defmodule RetWeb.HealthController do
     from(h in Ret.Hub, limit: 1) |> Ret.Repo.one!()
 
     # Check page cache
-    true = Ret.PageOriginWarmer.chunks_for_page(:hubs, "index.html") |> elem(1) |> Enum.count() > 0
-    true = Ret.PageOriginWarmer.chunks_for_page(:hubs, "hub.html") |> elem(1) |> Enum.count() > 0
-    true = Ret.PageOriginWarmer.chunks_for_page(:spoke, "index.html") |> elem(1) |> Enum.count() > 0
+    true = Cachex.get(:page_chunks, {:hubs, "index.html"}) |> elem(1) |> Enum.count() > 0
+    true = Cachex.get(:page_chunks, {:hubs, "hub.html"}) |> elem(1) |> Enum.count() > 0
+    true = Cachex.get(:page_chunks, {:spoke, "index.html"}) |> elem(1) |> Enum.count() > 0
 
     # Check room routing
     true = Ret.RoomAssigner.get_available_host("") != nil
