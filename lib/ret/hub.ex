1199b0735edb0963a0ee23e299d753d9e874e983
Brian Peiris <brianpeiris@gmail.com>
2019-07-18 18:03:21 -0700

    Whoops, fix refactoring regression

--- lib/ret/hub.ex
+++ lib/ret/hub.ex
@@ -320,7 +320,7 @@ defmodule Ret.Hub do
 
     @member_permissions
     |> Enum.reduce(0, fn {val, member_permission}, acc ->
-      if member_permissions[member_permission], do: val + acc, else: 0
+      if(member_permissions[member_permission], do: val, else: 0) + acc
     end)
   end
 
@@ -424,13 +424,13 @@ end
 defimpl Canada.Can, for: Ret.OAuthProvider do
   alias Ret.{Hub}
   @object_actions [:spawn_and_move_media, :spawn_camera, :spawn_drawing, :pin_objects]
-  @all_actions [:update_hub, :close_hub, :embed_hub, :kick_users, :mute_users] ++ @object_actions
+  @special_actions [:update_hub, :close_hub, :embed_hub, :kick_users, :mute_users]
 
   # Always deny access to non-enterable hubs
   def can?(%Ret.OAuthProvider{}, :join_hub, %Ret.Hub{entry_mode: :deny}), do: false
 
-  # OAuthProvider users cannot perform all actions
-  def can?(%Ret.OAuthProvider{}, action, %Ret.Hub{}) when action in @all_actions,
+  # OAuthProvider users cannot perform special actions
+  def can?(%Ret.OAuthProvider{}, action, %Ret.Hub{}) when action in @special_actions,
     do: false
 
   def can?(%Ret.OAuthProvider{} = oauth_provider, :join_hub, %Ret.Hub{hub_bindings: hub_bindings})
