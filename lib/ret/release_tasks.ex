fe54310b58e2a5139112a67493167c512a6847a2
Greg Fodor <gfodor@gmail.com>
2019-09-03 21:22:47 +0000

    Rename key because of stuck lock

--- lib/ret/release_tasks.ex
+++ lib/ret/release_tasks.ex
@@ -2,7 +2,7 @@ defmodule Ret.ReleaseTasks do
   def migrate do
     {:ok, _} = Application.ensure_all_started(:ret)
 
-    Ret.Locking.exec_after_session_lock("ret_migrations", fn ->
+    Ret.Locking.exec_after_session_lock("ret_migration", fn ->
       Ecto.Migrator.run(Ret.Repo, migrations_path(:ret), :up, all: true)
     end)
   end
