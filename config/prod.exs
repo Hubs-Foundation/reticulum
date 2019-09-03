23ba557f2421448c716274679fb65c09ac397dbc
Greg Fodor <gfodor@gmail.com>
2019-09-03 22:28:07 +0000

    Formatting

--- config/prod.exs
+++ config/prod.exs
@@ -92,12 +92,17 @@ config :ret, Ret.Scheduler,
     # Flush stats to db every 5 minutes
     {{:cron, "*/5 * * * *"}, {Ret.StatsJob, :save_node_stats, []}},
 
-    {"0 10 * * *", {Ret.Storage, :vacuum, []}},
-    {"5 10 * * *", {Ret.Storage, :demote_inactive_owned_files, []}},
-    {"10 10 * * *", {Ret.LoginToken, :expire_stale, []}},
-    {"15 10 * * *", {Ret.Hub, :vacuum_entry_codes, []}},
-    {"20 10 * * *", {Ret.Hub, :vacuum_hosts, []}},
-    {"25 10 * * *", {Ret.CachedFile, :vacuum, []}}
+    {{:cron, "0 10 * * *"}, {Ret.Storage, :vacuum, []}},
+
+    {{:cron, "5 10 * * *"}, {Ret.Storage, :demote_inactive_owned_files, []}},
+
+    {{:cron, "10 10 * * *"}, {Ret.LoginToken, :expire_stale, []}},
+
+    {{:cron, "15 10 * * *"}, {Ret.Hub, :vacuum_entry_codes, []}},
+
+    {{:cron, "20 10 * * *"}, {Ret.Hub, :vacuum_hosts, []}},
+
+    {{:cron, "25 10 * * *"}, {Ret.CachedFile, :vacuum, []}}
   ]
 
 config :ret, RetWeb.Plugs.HeaderAuthorization, header_name: "x-ret-admin-access-key"
