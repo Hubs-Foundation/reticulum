7ba0b7d1d66d773413585a264b05dc483e44c52a
Greg Fodor <gfodor@gmail.com>
2019-09-03 21:33:17 +0000

    Fix up migrations due to ecto upgrade

--- priv/repo/migrations/20181128022000_add_min_token_issued_at_to_account.exs
+++ priv/repo/migrations/20181128022000_add_min_token_issued_at_to_account.exs
@@ -2,7 +2,7 @@ defmodule Ret.Repo.Migrations.AddMinTokenIssuedAtToAccount do
   use Ecto.Migration
 
   def change do
-    epoch = Ecto.DateTime.from_unix!(0, :seconds) |> Ecto.DateTime.to_iso8601()
+    epoch = DateTime.from_unix!(0, :seconds) |> DateTime.to_iso8601() |> DateTime.truncate(:second)
 
     alter table("accounts") do
       add(:min_token_issued_at, :utc_datetime, default: epoch, null: false)
