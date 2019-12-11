4c4dc720f62d2a7c76f5935938f390fd42f08560
Greg Fodor <gfodor@gmail.com>
2019-12-11 05:55:01 +0000

    Specify ret port

--- lib/ret_web/plugs/add_csp.ex
+++ lib/ret_web/plugs/add_csp.ex
@@ -27,15 +27,16 @@ defmodule RetWeb.Plugs.AddCSP do
     janus_port = Application.get_env(:ret, Ret.JanusLoadStatus)[:janus_port]
     ret_host = Ret.Meta.get_meta(include_repo: false)[:phx_host]
     ret_domain = ret_host |> String.split(".") |> Enum.take(-2) |> Enum.join(".")
+    ret_port = RetWeb.Endpoint.url |> Keyword.get(:port)
     is_subdomain = ret_host |> String.split(".") |> length > 2
     link_url = config_url(:link_url)
     thumbnail_url = config_url(:thumbnail_url)
                     || cors_proxy_url |> String.replace("cors-proxy", "nearspark") # legacy
 
     wss_connect = if is_subdomain do
-      "wss://*.#{ret_domain} wss://*.#{ret_domain}:#{janus_port}"
+      "wss://*.#{ret_domain}:#{ret_port} wss://*.#{ret_domain}:#{janus_port}"
     else
-      "wss://#{ret_host}:#{janus_port} wss://#{ret_host}"
+      "wss://#{ret_host}:#{janus_port} wss://#{ret_host}:#{ret_port}"
     end
 
     "default-src 'none'; manifest-src 'self'; script-src #{assets_url} 'self' 'sha256-ViVvpb0oYlPAp7R8ZLxlNI6rsf7E7oz8l1SgCIXgMvM=' 'sha256-hsbRcgUBASABDq7qVGVTpbnWq/ns7B+ToTctZFJXYi8=' 'sha256-MIpWPgYj31kCgSUFc0UwHGQrV87W6N5ozotqfxxQG0w=' 'sha256-buF6N8Z4p2PuaaeRUjm7mxBpPNf4XlCT9Fep83YabbM=' 'sha256-/S6PM16MxkmUT7zJN2lkEKFgvXR7yL4Z8PCrRrFu4Q8=' https://www.google-analytics.com #{assets_url} https://aframe.io https://www.youtube.com https://s.ytimg.com 'unsafe-eval'; prefetch-src 'self' #{assets_url}; child-src 'self' blob:; worker-src #{assets_url} 'self' blob:; font-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net https://fonts.gstatic.com https://cdn.aframe.io #{assets_url} #{cors_proxy_url}; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net #{cors_proxy_url} #{assets_url} 'unsafe-inline'; connect-src 'self' #{cors_proxy_url} #{assets_url} #{link_url} https://dpdb.webvr.rocks #{thumbnail_url} #{wss_connect} https://cdn.aframe.io https://www.youtube.com https://api.github.com data: blob:; img-src 'self' https://www.google-analytics.com #{assets_url} #{cors_proxy_url} #{thumbnail_url} https://cdn.aframe.io https://www.youtube.com https://user-images.githubusercontent.com https://cdn.jsdelivr.net data: blob:; media-src 'self' #{cors_proxy_url} #{assets_url} #{thumbnail_url} https://www.youtube.com *.googlevideo.com data: blob:; frame-src https://www.youtube.com https://docs.google.com 'self'; base-uri 'none'; form-action 'self';"
