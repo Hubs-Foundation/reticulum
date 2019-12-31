0f01f08b1927cef49cd63719a2a3f2a92675383c
Greg Fodor <gfodor@gmail.com>
2019-12-31 01:43:23 +0000

    Drop temporary migration of schema migrations table now that it is deployed

--- lib/ret/release_tasks.ex
+++ lib/ret/release_tasks.ex
@@ -5,9 +5,6 @@ defmodule Ret.ReleaseTasks do
     Ret.Locking.exec_if_session_lockable("ret_migration", fn ->
       Ecto.Adapters.SQL.query!(Ret.Repo, "CREATE SCHEMA IF NOT EXISTS ret0")
 
-      # Legacy, migrations table used to be under public schema
-      Ecto.Adapters.SQL.query!(Ret.Repo, "ALTER TABLE IF EXISTS public.schema_migrations SET SCHEMA ret0")
-
       Ecto.Migrator.run(Ret.Repo, migrations_path(:ret), :up, all: true, prefix: "ret0")
     end)
   end
