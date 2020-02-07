806a03367a8f2e35d4f5e0bd7cecfc7190581044
Greg Fodor <gfodor@gmail.com>
2020-02-07 02:00:22 +0000

    Revert "Add ability to specify perms token for checking hub creation permission"
    This reverts commit 77548cf93727aef6155b891dc9b360f91cbf69cf.

--- lib/ret_web/controllers/api/v1/hub_controller.ex
+++ lib/ret_web/controllers/api/v1/hub_controller.ex
@@ -1,7 +1,7 @@
 defmodule RetWeb.Api.V1.HubController do
   use RetWeb, :controller
 
-  alias Ret.{Account, Hub, PermsToken, Scene, SceneListing, Repo}
+  alias Ret.{Account, Hub, Scene, SceneListing, Repo}
 
   # Limit to 1 TPS
   plug(RetWeb.Plugs.RateLimit)
@@ -14,7 +14,7 @@ defmodule RetWeb.Api.V1.HubController do
 
     %Hub{}
     |> Hub.changeset(scene, params["hub"])
-    |> exec_create(conn, params)
+    |> exec_create(conn)
   end
 
   def create(conn, %{"hub" => _hub_params} = params) do
@@ -22,24 +22,26 @@ defmodule RetWeb.Api.V1.HubController do
 
     %Hub{}
     |> Hub.changeset(scene_listing, params["hub"])
-    |> exec_create(conn, params)
+    |> exec_create(conn)
   end
 
-  defp exec_create(hub_changeset, conn, params) do
+  defp exec_create(hub_changeset, conn) do
     account = Guardian.Plug.current_resource(conn)
 
-    if can_create_hub?(account, params["perms_token"]) do
-      {result, hub} =
-        hub_changeset
-        |> Hub.add_account_to_changeset(account)
-        |> Repo.insert()
+    case Account.get_global_perms_for_account(account) do
+      %{hub_create: true} ->
+        {result, hub} =
+          hub_changeset
+          |> Hub.add_account_to_changeset(account)
+          |> Repo.insert()
 
-      case result do
-        :ok -> render(conn, "create.json", hub: hub)
-        :error -> conn |> send_resp(422, "invalid hub")
-      end
-    else
-      conn |> send_resp(401, "unauthorized")
+        case result do
+          :ok -> render(conn, "create.json", hub: hub)
+          :error -> conn |> send_resp(422, "invalid hub")
+        end
+
+      _ ->
+        conn |> send_resp(401, "unauthorized")
     end
   end
 
@@ -51,15 +53,4 @@ defmodule RetWeb.Api.V1.HubController do
 
     conn |> send_resp(200, "OK")
   end
-
-  defp can_create_hub?(_account, perms_token) when perms_token != nil do
-    case PermsToken.decode_and_verify(perms_token) do
-      {:ok, %{"create_hub" => true}} -> true
-      _ -> false
-    end
-  end
-
-  # Deprecated, old clients which do not pass token fall back on dynamic permissions
-  defp can_create_hub?(account, _perms_token),
-    do: account |> Account.get_global_perms_for_account() |> Map.get(:hub_create)
 end
