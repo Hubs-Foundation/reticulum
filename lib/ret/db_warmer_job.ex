566a2bc6c36c708c3e108c73aa7a3531e2d556fe
Greg Fodor <gfodor@gmail.com>
2020-04-16 23:47:30 +0000

    Do not query database for coturn if nobody is connected

--- lib/ret/db_warmer_job.ex
+++ lib/ret/db_warmer_job.ex
@@ -4,9 +4,7 @@ defmodule Ret.DbWarmerJob do
   # Routine run to keep database up when there are any CCU.
   # For AWS aurora, database shuts down if not being used.
   def warm_db_if_has_ccu do
-    member_count = RetWeb.Presence.present_member_count()
-
-    if member_count > 0 do
+    if RetWeb.Presence.has_present_members?() do
       # Ping DB to keep up
       from(h in Ret.Hub, limit: 0) |> Ret.Repo.all()
     end
