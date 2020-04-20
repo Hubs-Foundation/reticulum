48ed81ef6e1ba1f06cbeb0938a581ef8d26e15d7
Greg Fodor <gfodor@gmail.com>
2020-04-20 18:27:25 +0000

    Revert "Add coturn user create"
    This reverts commit eccaa59d3a5f201c74217985345267f8c002c2ca.

--- priv/repo/migrations/20200413202224_add_coturn_schema.exs
+++ priv/repo/migrations/20200413202224_add_coturn_schema.exs
@@ -2,8 +2,6 @@ defmodule Ret.Repo.Migrations.AddCoturnSchema do
   use Ecto.Migration
 
   def up do
-    coturn_password = Application.get_env(:ret, __MODULE__)[:coturn_password]
-
     execute("create schema if not exists coturn")
 
     execute(
@@ -17,35 +15,6 @@ defmodule Ret.Repo.Migrations.AddCoturnSchema do
     execute(
       "create table coturn.denied_peer_ip (realm varchar(127), ip_range varchar(256), inserted_at timestamp, updated_at timestamp)"
     )
-
-    if coturn_password do
-      execute("""
-      DO
-      $do$
-      BEGIN
-         IF NOT EXISTS (
-            SELECT                       -- SELECT list can stay empty for this
-            FROM   pg_catalog.pg_roles
-            WHERE  rolname = 'coturn') THEN
-
-            CREATE ROLE coturn LOGIN PASSWORD '#{coturn_password}';
-         END IF;
-      END
-      $do$;
-      """)
-
-      execute("""
-      do 
-      $$ 
-      begin
-        execute format('grant connect on database %I to coturn', current_database());
-      end;
-      $$;
-      """)
-
-      execute("grant usage on schema coturn to coturn;")
-      execute("grant all privileges on all tables in schema coturn to coturn;")
-    end
   end
 
   def down do
