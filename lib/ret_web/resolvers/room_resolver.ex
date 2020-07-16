bd512d59d21ff91f2325838d3e1eedf2cbb6bc75
Robert Long <robert@robertlong.me>
2020-07-15 18:47:00 -0700

    Use a changeset error handling middleware

--- lib/ret_web/resolvers/room_resolver.ex
+++ lib/ret_web/resolvers/room_resolver.ex
@@ -6,23 +6,6 @@ defmodule RetWeb.Resolvers.RoomResolver do
   end
 
   def create_room(_parent, args, _resolutions) do
-    args
-    |> Hub.create()
-    |> case do
-      {:ok, room} ->
-        {:ok, room}
-
-      {:error, changeset} ->
-        {:error, extract_error_message(changeset)}
-    end
-  end
-
-  defp extract_error_message(changeset) do
-    Enum.map(changeset.errors, fn {field, {error, _details}} ->
-      [
-        field: field,
-        message: String.capitalize(error)
-      ]
-    end)
+    Hub.create(args)
   end
 end
--- lib/ret_web/schema.ex
+++ lib/ret_web/schema.ex
@@ -1,6 +1,11 @@
 defmodule RetWeb.Schema do
   use Absinthe.Schema
 
+  def middleware(middleware, _field, %{identifier: :mutation}) do
+    middleware ++ [RetWeb.Middlewares.HandleChangesetErrors]
+  end
+  def middleware(middleware, _field, _object), do: middleware
+
   import_types(Absinthe.Type.Custom)
   import_types(RetWeb.Schema.RoomTypes)
 
