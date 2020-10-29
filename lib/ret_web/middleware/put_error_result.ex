560da978a594be56dee0ada9ab30a82717cddd1f
John Shaughnessy <johnfshaughnessy@gmail.com>
2020-10-28 17:00:27 -0700

    Remove unused error message

--- lib/ret_web/middleware/put_error_result.ex
+++ lib/ret_web/middleware/put_error_result.ex
@@ -3,17 +3,6 @@ defmodule RetWeb.Middleware.PutErrorResult do
 
   import Absinthe.Resolution, only: [put_result: 2]
 
-  def put_error_result(resolution, :no_resource_found, _message) do
-    put_result(
-      resolution,
-      {:error,
-       [
-         type: :api_access_token_invalid_or_not_found,
-         message: "API access token is invalid or missing. Did you add an authorization: bearer <your_token> header?"
-       ]}
-    )
-  end
-
   def put_error_result(resolution, type, message) do
     put_result(
       resolution,
