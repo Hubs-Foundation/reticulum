4923a6668c34b9b1d7aab521232afaa55a206224
John Shaughnessy <johnfshaughnessy@gmail.com>
2020-11-16 21:00:29 -0800

    Remove max_page_size
    This parameter affects existing API calls.
    While I do not think it will cause any issues, I am removing
    it for now because I want the changes to be isolated to the
    new API as much as possible. I'd be happy to reintroduce this
    later as-is, or before release if it targets this API specifically.

--- lib/ret/repo.ex
+++ lib/ret/repo.ex
@@ -1,6 +1,6 @@
 defmodule Ret.Repo do
   use Ecto.Repo, otp_app: :ret, adapter: Ecto.Adapters.Postgres
-  use Scrivener, page_size: 20, max_page_size: 50
+  use Scrivener, page_size: 20
 
   def init(_, opts) do
     {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
