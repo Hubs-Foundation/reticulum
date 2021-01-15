95a17a71deb3c9d491f83e9f6dade04a3c027d13
John Shaughnessy <johnfshaughnessy@gmail.com>
2021-01-15 13:26:12 -0800

    lint

--- lib/ret/http_utils.ex
+++ lib/ret/http_utils.ex
@@ -24,7 +24,8 @@ defmodule Ret.HttpUtils do
   end
 
   def retry_until_success(verb, url, body \\ "", headers \\ [], cap_ms \\ 5_000, expiry_ms \\ 10_000) do
-    headers = headers ++ [{"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"}]
+    headers =
+      headers ++ [{"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"}]
 
     hackney_options =
       if module_config(:insecure_ssl) == true do
