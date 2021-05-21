9e10cd2a9ac5ea2d3dc00dd197658a7755c0a187
netpro2k <netpro2k@gmail.com>
2021-05-20 20:46:24 -0700

    Add amplify_audio permission

--- test/ret/hub_test.exs
+++ test/ret/hub_test.exs
@@ -28,7 +28,7 @@ defmodule Ret.HubTest do
     {:ok, hub} = %Hub{} |> Hub.changeset(scene, %{name: "Test Hub"}) |> Repo.insert()
     hub = hub |> Repo.preload([:hub_bindings, :hub_role_memberships])
 
-    %{join_hub: true, update_hub: false, close_hub: false, mute_users: false} =
+    %{join_hub: true, update_hub: false, close_hub: false, mute_users: false, amplify_audio: false} =
       hub |> Hub.perms_for_account(Ret.Account.account_for_email("non-creator@mozilla.com"))
   end
 
@@ -36,7 +36,8 @@ defmodule Ret.HubTest do
     {:ok, hub} = %Hub{} |> Hub.changeset(scene, %{name: "Test Hub"}) |> Repo.insert()
     hub = hub |> Repo.preload([:hub_bindings])
 
-    %{join_hub: true, update_hub: false, close_hub: false, mute_users: false} = hub |> Hub.perms_for_account(nil)
+    %{join_hub: true, update_hub: false, close_hub: false, mute_users: false, amplify_audio: false} =
+      hub |> Hub.perms_for_account(nil)
   end
 
   test "should deny entry for closed hub, allow entry for re-opened hub", %{scene: scene} do
@@ -63,7 +64,8 @@ defmodule Ret.HubTest do
 
     hub = hub |> Repo.preload([:hub_bindings, :hub_role_memberships])
 
-    %{join_hub: true, update_hub: true, close_hub: true, mute_users: true} = hub |> Hub.perms_for_account(account)
+    %{join_hub: true, update_hub: true, close_hub: true, mute_users: true, amplify_audio: true} =
+      hub |> Hub.perms_for_account(account)
   end
 
   test "should have creator assignment token if no account assigned", %{scene: scene} do
