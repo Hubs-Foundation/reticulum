f461db28af4c46468a74799e9aff6d3d42569b5d
Geng Tan <geng.tan@limeade.com>
2021-11-05 11:29:44 -0400

    cleanups

--- lib/ret_web/plugs/proxies.ex
+++ lib/ret_web/plugs/proxies.ex
@@ -1,11 +1,9 @@
 defmodule RetWeb.Plugs.PostgrestProxy do
   use Plug.Builder
-  pgrest_host = Application.get_env(:ret, :pgrest_host) || "localhost:3000"
-  plug ReverseProxyPlug, upstream: "http://#{pgrest_host}"
+  plug ReverseProxyPlug, upstream: "http://localhost:3000"
 end
 
 defmodule RetWeb.Plugs.ItaProxy do
   use Plug.Builder
-  ita_host = Application.get_env(:ret, :ita_host) || "localhost:6000"
-  plug ReverseProxyPlug, upstream: "http://#{ita_host}"
+  plug ReverseProxyPlug, upstream: "http://localhost:6000"
 end
