5ff5582a418a6e192071a0a9250d3d5cb5bbd4ed
Robin K Wilson <robin.kath.wilson@gmail.com>
2022-03-21 15:45:46 -0700

    filters in room ccu better

--- lib/ret_web/controllers/api/v1/presence_controller.ex
+++ lib/ret_web/controllers/api/v1/presence_controller.ex
@@ -3,7 +3,7 @@ defmodule RetWeb.Api.V1.PresenceController do
 
   # Get presence count
   def show(conn, _) do
-    count = RetWeb.Presence.present_session_count()
+    count = RetWeb.Presence.present_ccu_in_room_count()
 
     conn |> send_resp(200, %{count: count} |> Poison.encode!())
   end
