382ff648bc665e696c2cf367890d53bc3b5cd7c7
Robin K Wilson <robin.kath.wilson@gmail.com>
2022-06-27 12:47:52 -0700

    wip

--- lib/ret/storage_stat.ex
+++ lib/ret/storage_stat.ex
@@ -8,6 +8,7 @@ defmodule Ret.StorageStat do
   import Ecto.Query
   alias Ret.{StorageStat, Repo}
   require NaiveDateTime
+  require Logger
 
   schema "storage_stats" do
     field(:measured_at, :utc_datetime)
@@ -28,67 +29,49 @@ defmodule Ret.StorageStat do
   # Adds current storage_stat
   # storage_blocks is in # units of 512 bytes
   def save_storage_stat(storage_blocks) do
-    if System.get_env("TURKEY_MODE") do
-      with node_id <- Node.self() |> to_string,
-           measured_at <- NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
-           present_storage_blocks <- storage_blocks do
-        %StorageStat{}
-        |> changeset(%{
-          node_id: node_id,
-          measured_at: measured_at,
-          present_storage_blocks: present_storage_blocks
-        })
-        |> Repo.insert()
-      end
+    Logger.warn("Saving storage: #{storage_blocks}")
+    # if System.get_env("TURKEY_MODE") do
+    with node_id <- Node.self() |> to_string,
+         measured_at <- NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
+         present_storage_blocks <- storage_blocks do
+      %StorageStat{}
+      |> changeset(%{
+        node_id: node_id,
+        measured_at: measured_at,
+        present_storage_blocks: present_storage_blocks
+      })
+      |> Repo.insert()
     end
+
+    # end
   end
 
+  @seconds_in_a_day 24 * 60 * 60
   def get_list_of_today_values(utc_date) do
-    today_date = NaiveDateTime.to_date(utc_date)
+    {:ok, time} = Time.new(0, 0, 0, 0)
+
+    {:ok, today_date} = \
+      utc_date \
+      |> NaiveDateTime.to_date() \
+      |> NaiveDateTime.new(time)
+
+    next_day_date = today_date |> NaiveDateTime.add(@seconds_in_a_day, :second)
 
     # TODO Will this have conflicts with different node_ids? or count twice if share same db?
-    from(ss in StorageStat, where: NaiveDateTime.compare(NaiveDateTime.to_date(ss.measured_at), today_date) == :eq)
+    from(ss in StorageStat, where: ss.measured_at >= ^today_date and ss.measured_at < ^next_day_date)
     |> Repo.all()
   end
 
   # TODO better verification of list of %StorageStat
-  defp storage_stat_list_to_unit_list(list_storage_stats) do
+  def storage_stat_list_to_unit_list(list_storage_stats) do
     list_storage_stats
     |> Enum.map(fn ss -> ss.present_storage_blocks end)
   end
 
-  defp get_average(unit_list) do
-    count = Kernel.length(unit_list)
-
-    sum =
-      unit_list
-      |> Enum.map(fn ss -> ss.present_storage_blocks end)
-      |> Enum.sum()
-
-    sum / count
-  end
-
-  defp get_max(unit_list) do
-    unit_list
-    |> Enum.map_reduce(unit_list, 0, fn u, acc ->
-      if acc > u do
-        acc
-      else
-        u
-      end
-    end)
-  end
-
-  defp get_min(unit_list) do
-    first_unit = Enum.at(unit_list, 0)
-
-    unit_list
-    |> Enum.map_reduce(unit_list, first_unit, fn u, acc ->
-      if acc > u do
-        acc
-      else
-        u
-      end
-    end)
+  def get_stats(unit_list) do
+    avg = Enum.sum(unit_list) / Kernel.length(unit_list)
+    max = Enum.max(unit_list)
+    min = Enum.min(unit_list)
+    %{avg: avg, max: max, min: min}
   end
 end
