5400df589d36211532c801bfdb0e51e2f5b38faa
Robin K Wilson <robin.kath.wilson@gmail.com>
2022-06-29 09:46:45 -0700

    return in kilobytes

--- lib/ret/storage_used.ex
+++ lib/ret/storage_used.ex
@@ -1,24 +1,23 @@
 defmodule Ret.StorageUsed do
   use Cachex.Warmer
   use Retry
-  require Logger
 
   def interval do
     # `du` command takes longer set the interval for longer
     if System.get_env("TURKEY_MODE"), do: :timer.minutes(10), else: :timer.minutes(5)
   end
 
-  @blocks_to_bytes 1024
   def execute(_state) do
     storage_path = Application.get_env(:ret, Ret.Storage)[:storage_path]
 
     if System.get_env("TURKEY_MODE") do
       # Yes TURKEY_MODE use `du`
-      case System.cmd("du", ["-s", storage_path]) do
+      # Return in kilobytes
+      case System.cmd("du", ["-ks", storage_path]) do
         {line, 0} ->
-          {blocks, _remainder_of_binary} = String.split(line, "\t") |> Enum.at(0) |> Integer.parse()
+          {kb, _remainder_of_binary} = String.split(line, "\t") |> Enum.at(0) |> Integer.parse()
 
-          {:ok, [{:storage_used, blocks * @blocks_to_bytes}]}
+          {:ok, [{:storage_used, kb}]}
 
         {_fail, 1} ->
           {:ok, [{:storage_used, 0}]}
