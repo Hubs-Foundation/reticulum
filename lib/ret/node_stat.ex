bde36de8f2bc0454c127d51c9b10e33a6ef7c9f8
Robin K Wilson <robin.kath.wilson@gmail.com>
2022-07-12 18:08:43 -0700

    Max ccu, seed method for local testing, presence controller

--- lib/ret/node_stat.ex
+++ lib/ret/node_stat.ex
@@ -1,8 +1,9 @@
 defmodule Ret.NodeStat do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
 
-  alias Ret.NodeStat
+  alias Ret.{NodeStat, Repo}
   @schema_prefix "ret0"
   @primary_key false
 
@@ -22,4 +23,63 @@ defmodule Ret.NodeStat do
       :present_rooms
     ])
   end
+
+  def max_ccu_for_timerange(start_time, end_time) do
+    max_ccu =
+      from(ns in NodeStat,
+        select: max(ns.present_sessions),
+        where: ns.measured_at >= ^start_time and ns.measured_at < ^end_time
+      )
+      |> Repo.one()
+
+    if max_ccu === nil, do: 0, else: max_ccu
+  end
+
+  # TODO REMOVE
+  def seed_node_stats() do
+    # Convert to dates
+    today_date = NaiveDateTime.utc_now() |> NaiveDateTime.to_date()
+    tomorrow_date = today_date |> Date.add(1)
+
+    # Convert to dates with times
+    {:ok, time} = Time.new(0, 0, 0, 0)
+    {:ok, start_date_time} = today_date |> NaiveDateTime.new(time)
+    {:ok, end_date_time} = tomorrow_date |> NaiveDateTime.new(time)
+
+    # Add value
+    today = start_date_time
+
+    Enum.each([0, 10, 5], fn count ->
+      {:ok, _} =
+        with node_id <- Node.self() |> to_string,
+             measured_at <- start_date_time |> NaiveDateTime.truncate(:second),
+             present_sessions <- count,
+             present_rooms <- 1 do
+          %NodeStat{}
+          |> NodeStat.changeset(%{
+            node_id: node_id,
+            measured_at: measured_at,
+            present_sessions: present_sessions,
+            present_rooms: present_rooms
+          })
+          |> Repo.insert()
+        end
+    end)
+  end
+
+  def get_times() do
+    # Convert to dates
+    today_date = NaiveDateTime.utc_now() |> NaiveDateTime.to_date() |> Date.add(-2)
+    tomorrow_date = today_date |> Date.add(-1)
+
+    # Convert to dates with times
+    {:ok, time} = Time.new(0, 0, 0, 0)
+    {:ok, start_date_time} = today_date |> NaiveDateTime.new(time)
+    {:ok, end_date_time} = tomorrow_date |> NaiveDateTime.new(time)
+
+    %{start_date_time: start_date_time, end_date_time: end_date_time}
+  end
+
+  # %{start_date_time: start_date_time, end_date_time: end_date_time} = Ret.NodeStat.get_times()
+  # Ret.NodeStat.max_ccu_for_timerange(start_date_time, end_date_time)
 end
--- lib/ret_web/controllers/api-internal/v1/presence_controller.ex
+++ lib/ret_web/controllers/api-internal/v1/presence_controller.ex
@@ -1,10 +1,16 @@
 defmodule RetWeb.ApiInternal.V1.PresenceController do
   use RetWeb, :controller
+  alias Ret.NodeStat
 
   # Get presence count
   def show(conn, _) do
     count = RetWeb.Presence.present_ccu_in_room_count()
 
     conn |> send_resp(200, %{count: count} |> Poison.encode!())
+  end
+
+  def daily_max(conn, %{start_time: start_time, end_time: end_time}) do
+    max = NodeStat.max_ccu_for_timerange(start_time, end_time)
+    conn |> send_resp(200, %{max_ccu: max} |> Poison.encode!())
   end
 end
