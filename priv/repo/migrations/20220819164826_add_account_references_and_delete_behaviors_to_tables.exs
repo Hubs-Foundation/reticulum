a5998a39d43519aacdf4a4be349e10a5655410dc
Brian Peiris <brianpeiris@gmail.com>
2022-08-29 10:46:55 -0400

    Reversible foreign key migration

--- priv/repo/migrations/20220819164826_add_account_references_and_delete_behaviors_to_tables.exs
+++ priv/repo/migrations/20220819164826_add_account_references_and_delete_behaviors_to_tables.exs
@@ -1,7 +1,7 @@
 defmodule Ret.Repo.Migrations.AddAccountReferencesAndDeleteBehaviorToTables do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:oauth_providers) do
       modify(:account_id, references(:accounts, column: :account_id, on_delete: :delete_all))
     end
@@ -10,6 +10,16 @@ defmodule Ret.Repo.Migrations.AddAccountReferencesAndDeleteBehaviorToTables do
 
     alter table(:api_credentials) do
       modify(:account_id, references(:accounts, column: :account_id, on_delete: :delete_all))
+    end
+  end
+
+  def down do
+    execute("alter table oauth_providers drop constraint oauth_providers_account_id_fkey")
+
+    execute("alter table api_credentials drop constraint api_credentials_account_id_fkey")
+
+    alter table(:api_credentials) do
+      modify(:account_id, references(:accounts, column: :account_id, on_delete: :nothing))
     end
   end
 end
