bf8c924d06c241fd70fcf0d1f98447a7fed4b13c
Brian Peiris <brianpeiris@gmail.com>
2022-09-01 13:56:44 -0400

    Cascade account deletion to hubs and associated entities

---
+++ priv/repo/migrations/20220819164826_add_foreign_references_and_delete_behaviors_to_tables.exs
@@ -0,0 +1,54 @@
+defmodule Ret.Repo.Migrations.AddForeignReferencesAndDeleteBehaviorToTables do
+  use Ecto.Migration
+
+  @tables_missing_references [
+    # table, foreign_table, column
+    {:hub_bindings, :hubs, :hub_id},
+    {:hub_invites, :hubs, :hub_id},
+    {:oauth_providers, :accounts, :account_id},
+    {:web_push_subscriptions, :hubs, :hub_id}
+  ]
+
+  @tables_missing_on_delete_action [
+    # table, column, foreign_table, foreign_column
+    {:account_favorites, :account_id, :accounts, :account_id},
+    {:api_credentials, :account_id, :accounts, :account_id},
+    {:hub_role_memberships, :hub_id, :hubs, :hub_id},
+    {:hubs, :created_by_account_id, :accounts, :account_id},
+    {:room_objects, :account_id, :accounts, :account_id}
+  ]
+
+  def up do
+    for {table, foreign_table, column} <- @tables_missing_references do
+      add_foreign_reference(table, column, foreign_table, column, on_delete: :delete_all)
+    end
+
+    for {table, column, foreign_table, foreign_column} <- @tables_missing_on_delete_action do
+      execute_drop_foreign_constraint(table, column)
+
+      add_foreign_reference(table, column, foreign_table, foreign_column, on_delete: :delete_all)
+    end
+  end
+
+  def down do
+    for {table, _foreign_table, column} <- @tables_missing_references do
+      execute_drop_foreign_constraint(table, column)
+    end
+
+    for {table, column, foreign_table, foreign_column} <- @tables_missing_on_delete_action do
+      execute_drop_foreign_constraint(table, column)
+
+      add_foreign_reference(table, column, foreign_table, foreign_column, on_delete: :nothing)
+    end
+  end
+
+  defp execute_drop_foreign_constraint(table, column) do
+    execute("alter table #{table} drop constraint #{table}_#{column}_fkey")
+  end
+
+  defp add_foreign_reference(table, column, foreign_table, foreign_column, on_delete: on_delete_action) do
+    alter table(table) do
+      modify(column, references(foreign_table, column: foreign_column, on_delete: on_delete_action))
+    end
+  end
+end
--- test/ret_test.exs
+++ test/ret_test.exs
@@ -2,7 +2,22 @@ defmodule RetTest do
   use Ret.DataCase
   import Ecto.Query, only: [from: 2]
   import Ret.TestHelpers
-  alias Ret.{Account, Api, Identity, Login, OAuthProvider, Repo}
+
+  alias Ret.{
+    Account,
+    AccountFavorite,
+    Api,
+    Hub,
+    HubBinding,
+    HubInvite,
+    HubRoleMembership,
+    Identity,
+    Login,
+    OAuthProvider,
+    Repo,
+    RoomObject,
+    WebPushSubscription
+  }
 
   describe "account deletion" do
     test "deletes account, login, identity, oauthproviders, and api_credentials" do
@@ -33,11 +48,92 @@ defmodule RetTest do
       assert 0 === count(OAuthProvider, test_account)
       assert 0 === count(Api.Credentials, test_account)
     end
+
+    test "deletes hub and associated entities" do
+      {:ok, admin_account: admin_account} = create_admin_account("admin")
+      test_account = create_account("test")
+      test_hub_member_account = create_account("test_member")
+
+      {:ok, hub} =
+        Repo.insert(%Hub{
+          name: "test hub",
+          slug: "fake test slug",
+          created_by_account: test_account
+        })
+
+      Repo.insert(%HubBinding{
+        hub: hub,
+        type: :discord,
+        community_id: "fake-community-id",
+        channel_id: "fake-channel-id"
+      })
+
+      Repo.insert(%AccountFavorite{
+        hub: hub,
+        account: test_account
+      })
+
+      Repo.insert(%HubInvite{
+        hub: hub,
+        hub_invite_sid: "fake-invite-sid"
+      })
+
+      Repo.insert(%HubRoleMembership{
+        hub: hub,
+        account: test_hub_member_account
+      })
+
+      Repo.insert(%RoomObject{
+        hub: hub,
+        account: test_account,
+        object_id: "fake object id",
+        gltf_node: "fake gltf node"
+      })
+
+      Repo.insert(%WebPushSubscription{
+        hub: hub,
+        endpoint: "fake-endpoint",
+        p256dh: "fake-key",
+        auth: "fake-auth-key"
+      })
+
+      1 = count_hubs(test_account)
+      1 = count(AccountFavorite, hub)
+      1 = count(HubBinding, hub)
+      1 = count(HubInvite, hub)
+      1 = count(HubRoleMembership, hub)
+      1 = count(RoomObject, hub)
+      1 = count(WebPushSubscription, hub)
+
+      assert :ok = Ret.delete_account(admin_account, test_account)
+
+      assert 0 === count_hubs(test_account)
+      assert 0 === count(AccountFavorite, hub)
+      assert 0 === count(HubBinding, hub)
+      assert 0 === count(HubInvite, hub)
+      assert 0 === count(HubRoleMembership, hub)
+      assert 0 === count(RoomObject, hub)
+      assert 0 === count(WebPushSubscription, hub)
+    end
   end
 
-  defp count(queryable, account) do
+  defp count_hubs(account) do
+    Ret.Repo.aggregate(
+      from(h in Hub, where: h.created_by_account_id == ^account.account_id),
+      :count
+    )
+  end
+
+  defp count(queryable, %Account{} = account) do
     Ret.Repo.aggregate(
       from(record in queryable, where: record.account_id == ^account.account_id),
+      :count
+    )
+  end
+
+  defp count(queryable, %Hub{} = hub) do
+    Ret.Repo.aggregate(
+      from(record in queryable, where: record.hub_id == ^hub.hub_id),
       :count
     )
   end
