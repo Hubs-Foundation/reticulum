2bd0881a4f96d9f2e50581ab54732de438d1d61e
Geng Tan <tanfarming@hotmail.com>
2022-10-10 10:08:06 -0400

    word

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -16,7 +16,7 @@ healthcheck &
 # TURKEY_MODE=1 exec /ret/bin/ret foreground
 python d2e.py > /ret/releases/1.0.0/runtime.exs
 
-echo "config.toml" && cat /ret/config.toml
-echo "runtime.exs" && cat /ret/releases/1.0.0/runtime.exs
+# echo "\nconfig.toml" && cat /ret/config.toml
+# echo "\nruntime.exs" && cat /ret/releases/1.0.0/runtime.exs
 
 TURKEY_MODE=1 exec /ret/bin/ret start
