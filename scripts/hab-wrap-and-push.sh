47b65d26e4a4237fd8f247ab8cad8e90dabc15f4
Geng Tan <tanfarming@hotmail.com>
2022-10-11 09:26:11 -0400

    words

--- scripts/hab-wrap-and-push.sh
+++ scripts/hab-wrap-and-push.sh
@@ -1,3 +1,35 @@
+
+set -e
+
+#BLDR_HAB_TOKEN='_Qk9YLTEKYmxkci0yMDE3M...'
+#BLDR_RET_TOKEN='_Qk9YLTEKYmxkci0yMDE5M...'
+#BLDR_RET_PUB_B64='U0lHLVBVQi0xCm1vemls...'
+
+### preps
+apk add curl
+org="biome-sh";repo="biome"
+ver=$(curl -s https://api.github.com/repos/$org/$repo/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}')
+dl="https://github.com/$org/$repo/releases/download/$ver/bio-${ver#"v"}-x86_64-linux.tar.gz"
+echo "[info] getting bio from: $dl" && curl -L -o bio.gz $dl && tar -xf bio.gz 
+cp ./bio /usr/bin/bio && bio --version
+
+bio origin key generate mozillareality
+habCacheKeyPath="/hab/cache/keys"
+echo "habCacheKeyPath: $habCacheKeyPath"
+mkdir -p $habCacheKeyPath
+echo $BLDR_HAB_TOKEN > $habCacheKeyPath/mozillareality_hab
+echo $BLDR_RET_TOKEN > $habCacheKeyPath/mozillareality_ret
+export HAB_ORIGIN=mozillareality
+export HAB_ORIGIN_KEYS=mozillareality_hab
+
+echo "### build hab pkg"
+export HAB_AUTH_TOKEN=$BLDR_HAB_TOKEN
+
+mkdir -p /repo/ret
+cp -r /ret/* /repo/ret/
+
+cd /repo
+cat > habitat/plan.sh << 'EOF'
 pkg_name=reticulum
 pkg_origin=mozillareality
 pkg_version="1.0.1"
@@ -63,3 +95,16 @@ do_strip() {
 do_end() {
     return 0
 }
+EOF
+bio pkg build --cache-key-path $habCacheKeyPath -k mozillareality .
+
+### upload
+echo "### upload hab pkg"
+export HAB_BLDR_URL="https://bldr.reticulum.io"
+export HAB_AUTH_TOKEN=$BLDR_RET_TOKEN
+export HAB_ORIGIN_KEYS=mozillareality_ret
+echo $BLDR_RET_PUB_B64 | base64 -d > /hab/cache/keys/mozillareality-20190117233449.pub
+# cat /hab/cache/keys/mozillareality-20190117233449.pub
+hart="/hab/cache/artifacts/$HAB_ORIGIN-reticulum*.hart"
+ls -lha $hart
+bio pkg upload $hart
