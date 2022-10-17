a421423a9abe0ac1f08011d7253594ab8fa6a4d5
Manuel Martin <manmartin@mozilla.com>
2022-10-17 13:34:27 +0200

    Enable migration rollback

--- priv/repo/migrations/20220908132321_add_voice_text_chat.exs
+++ priv/repo/migrations/20220908132321_add_voice_text_chat.exs
@@ -23,23 +23,21 @@ defmodule Ret.Repo.Migrations.AddFlyPermission do
   end
 
   def down do
-    # Probably don't ever want to revert this migration, but in case we do:
+    alter table("hubs") do
+      modify(:member_permissions, :integer,
+        default:
+          %{
+            spawn_and_move_media: true,
+            spawn_camera: true,
+            spawn_drawing: true,
+            pin_objects: true,
+            spawn_emoji: true,
+            fly: true
+          }
+          |> Ret.Hub.member_permissions_to_int()
+      )
+    end
 
-    # alter table("hubs") do
-    #   modify(:member_permissions, :integer,
-    #     default:
-    #       %{
-    #         spawn_and_move_media: true,
-    #         spawn_camera: true,
-    #         spawn_drawing: true,
-    #         pin_objects: true,
-    #         spawn_emoji: true,
-    #         fly: true
-    #       }
-    #       |> Ret.Hub.member_permissions_to_int()
-    #   )
-    # end
-
-    # execute("UPDATE ret0.hubs SET member_permissions = member_permissions & ~192;");
+    execute("UPDATE ret0.hubs SET member_permissions = member_permissions & ~192;");
   end
 end
