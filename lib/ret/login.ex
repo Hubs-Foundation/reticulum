42fbfa56fac131e43c03c29b7ce5c4b6b78a69f8
Robin Wilson <robin.kath.wilson@gmail.com>
2022-10-19 12:17:52 -0700

    wip adding logic to change identifier hash for account from email1 to email2

--- lib/ret/login.ex
+++ lib/ret/login.ex
@@ -1,13 +1,15 @@
 defmodule Ret.Login do
   use Ecto.Schema
+
   import Ecto.Changeset
+  alias Ret.{Repo, Account}
 
   @schema_prefix "ret0"
   @primary_key {:login_id, :id, autogenerate: true}
 
   schema "logins" do
     field(:identifier_hash, :string)
-    belongs_to(:account, Ret.Account, references: :account_id)
+    belongs_to(:account, Account, references: :account_id)
 
     timestamps()
   end
@@ -21,5 +23,25 @@ defmodule Ret.Login do
     |> validate_required([:identifier_hash])
     |> unique_constraint(:identifier_hash)
     |> put_assoc(:account, account)
+  end
+
+  def update_identifier_hash(%{old_email: old_email, new_email: new_email} = _) do
+    old_identifier_hash = Account.identifier_hash_for_email(old_email)
+    new_identifier_hash = Account.identifier_hash_for_email(new_email)
+
+    login_to_update =
+      Ret.Login
+      |> Repo.get_by(identifier_hash: old_identifier_hash)
+      |> Repo.one()
+
+    case login_to_update do
+      %Ret.Login{} ->
+        login_to_update
+        |> change(%{identifier_hash: new_identifier_hash})
+        |> Repo.update!()
+
+      nil ->
+        :error
+    end
   end
 end
