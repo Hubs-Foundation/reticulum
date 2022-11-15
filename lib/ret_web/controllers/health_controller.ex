bafdb5266845efb9270279952e835c065a6caa17
tanfarming <tanfarming@hotmail.com>
2022-11-14 22:30:35 -0500

    word

--- lib/ret_web/controllers/health_controller.ex
+++ lib/ret_web/controllers/health_controller.ex
@@ -2,7 +2,8 @@ defmodule RetWeb.HealthController do
   use RetWeb, :controller
   import Ecto.Query
 
-  def index(conn, _params) do
+  def index(conn, _params) do    
+    
     # Check database
     if module_config(:check_repo) do
       from(h in Ret.Hub, limit: 0) |> Ret.Repo.all()
@@ -16,7 +17,13 @@ defmodule RetWeb.HealthController do
     # Check room routing
     true = Ret.RoomAssigner.get_available_host("") != nil
 
-    send_resp(conn, 200, "ok")
+    # check storage
+    if System.get_env("TURKEY_MODE") do
+      with File.ls!("#{module_config(:storage_path)}/") do
+        {:ok} -> send_resp(conn, 200, "ok")
+      else
+        _ -> send_resp(conn, 500, "ERROR")
+    end
   end
 
   defp module_config(key) do
