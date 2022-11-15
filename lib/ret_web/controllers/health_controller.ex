04f6db7f46909ee3199204705ce2aa6f4de48db9
tanfarming <tanfarming@hotmail.com>
2022-11-14 23:06:44 -0500

    word

--- lib/ret_web/controllers/health_controller.ex
+++ lib/ret_web/controllers/health_controller.ex
@@ -18,8 +18,9 @@ defmodule RetWeb.HealthController do
 
     # check storage
     if System.get_env("TURKEY_MODE") do
-      case File.ls("#{module_config(:storage_path)}/") do
-        {:ok} -> {send_resp(conn, 200, "ok")}
+      path=Application.get_env(:ret, Storage)[:storage_path]
+      case File.ls("#{path}/") do
+        {:ok, _} -> {send_resp(conn, 200, "ok")}
         {_, ex} -> {send_resp(conn, 500, ex)}
       end
     end
