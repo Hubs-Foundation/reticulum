1c272167867c64114fb9b3e0123c8569a0046ee9
tanfarming <tanfarming@hotmail.com>
2022-11-16 10:12:17 -0500

    word

--- lib/ret_web/controllers/api-internal/v1/gen_user_token_controller.ex
+++ lib/ret_web/controllers/api-internal/v1/gen_user_token_controller.ex
@@ -2,14 +2,11 @@ defmodule RetWeb.ApiInternal.V1.GenUserTokenController do
   require Logger
 
   use RetWeb, :controller
-  
-  alias Ret.{Account}
-  
-  @starts_with_https ~r/^https:\/\//
-  def update(conn, %{"old_email" => old_email, "new_email" => new_email}) do    
+    
+  def update(conn, %{"email" => email}) do    
     try do
-      acct = Account.account_for_email(email) 
-      token = TokenUtils.gen_token_for_account(acct)
+      acct = Ret.Account.account_for_email(email) 
+      token = Ret.Api.TokenUtils.gen_token_for_account(acct)
       send_resp(conn, 200, token)
     rescue
       exception ->
