cb1610615f40e16f3f2c1f30309cc1cf204503ff
tanfarming <tanfarming@hotmail.com>
2022-11-16 11:59:31 -0500

    word

--- lib/ret_web/controllers/api-internal/v1/gen_user_token_controller.ex
+++ lib/ret_web/controllers/api-internal/v1/gen_user_token_controller.ex
@@ -5,15 +5,23 @@ defmodule RetWeb.ApiInternal.V1.GenUserTokenController do
     
   def post(conn, %{"email" => email}) when is_binary(email) do
     IO.puts("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    IO.puts(inspect(conn))
+    # IO.puts(inspect(conn))
     IO.puts("email: #{inspect(email)}")
-    try do
-      acct = Ret.Account.account_for_email(email) 
-      token = Ret.Api.TokenUtils.gen_token_for_account(acct)
-      send_resp(conn, 200, token)
-    rescue
-      exception ->
-        send_resp(conn, 500, exception)
-    end
+    
+    acct = Ret.Account.account_for_email(email) 
+    IO.puts("acct: #{inspect(acct)}")
+    
+    token = Ret.Api.TokenUtils.gen_token_for_account(acct)    
+    IO.puts("token: #{inspect(token)}")
+    
+    # try do
+    #   acct = Ret.Account.account_for_email(email) 
+    #   token = Ret.Api.TokenUtils.gen_token_for_account(acct)
+    #   send_resp(conn, 200, token)
+    # rescue
+    #   exception ->
+    #     send_resp(conn, 500, exception)
+    # end
+    send_resp(conn, 200, token)
   end
 end
