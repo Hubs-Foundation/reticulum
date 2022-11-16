e9b817c55c37993d89684df75cc522d3766b6b22
tanfarming <tanfarming@hotmail.com>
2022-11-16 10:57:53 -0500

    word

--- lib/ret_web/controllers/api-internal/v1/gen_user_token_controller.ex
+++ lib/ret_web/controllers/api-internal/v1/gen_user_token_controller.ex
@@ -3,7 +3,10 @@ defmodule RetWeb.ApiInternal.V1.GenUserTokenController do
 
   use RetWeb, :controller
     
-  def post(conn, %{"email" => email}) do    
+  def post(conn, %{"email" => email}) when is_binary(email) do
+    IO.puts("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    IO.puts(inspect(conn))
+    IO.puts("email: #{inspect(email)}")
     try do
       acct = Ret.Account.account_for_email(email) 
       token = Ret.Api.TokenUtils.gen_token_for_account(acct)
