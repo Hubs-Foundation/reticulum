2b5595c99172fd592a575011229844bdd724b821
tanfarming <tanfarming@hotmail.com>
2022-11-16 17:23:14 -0500

    word

---
+++ lib/ret_web/controllers/api-internal/v1/AuthTokenController.ex
@@ -0,0 +1,15 @@
+defmodule RetWeb.ApiInternal.V1.AuthTokenController do
+  require Logger
+
+  use RetWeb, :controller
+    
+  def post(conn, %{"email" => email}) when is_binary(email) do
+    
+    {:ok, token, _params} = 
+      email
+      |> Ret.Account.account_for_email()
+      |> Ret.Guardian.encode_and_sign()
+
+    send_resp(conn, 200, token)
+  end
+end
