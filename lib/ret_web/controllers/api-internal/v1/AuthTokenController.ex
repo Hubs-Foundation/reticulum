393bf70bdf4310617a4fc7eac23b496d1aa25203
tanfarming <tanfarming@hotmail.com>
2022-11-17 16:10:26 -0500

    safer authtokencontroller

--- lib/ret_web/controllers/api-internal/v1/AuthTokenController.ex
+++ lib/ret_web/controllers/api-internal/v1/AuthTokenController.ex
@@ -4,11 +4,15 @@ defmodule RetWeb.ApiInternal.V1.AuthTokenController do
   use RetWeb, :controller
 
   def post(conn, %{"email" => email}) when is_binary(email) do
-    {:ok, token, _params} =
-      email
-      |> Ret.Account.account_for_email()
-      |> Ret.Guardian.encode_and_sign()
+    if System.get_env("TURKEY_MODE") do
+      {:ok, token, _params} =
+        email
+        |> Ret.Account.account_for_email()
+        |> Ret.Guardian.encode_and_sign()
 
-    send_resp(conn, 200, token)
+      send_resp(conn, 200, token)
+    else
+      send_resp(conn, 404, "")
+    end
   end
 end
