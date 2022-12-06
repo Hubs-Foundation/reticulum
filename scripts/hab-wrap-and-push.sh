b8b28ae7c39a6c5a29fd142f82791daed306882c
Geng Tan <tanfarming@hotmail.com>
2022-12-06 18:20:30 -0500

    Update hab-wrap-and-push.sh

--- scripts/hab-wrap-and-push.sh
+++ scripts/hab-wrap-and-push.sh
@@ -6,21 +6,20 @@ set -e
 #BLDR_RET_PUB_B64='U0lHLVBVQi0xCm1vemls...'
 
 ### preps
-apk add curl
 org="biome-sh";repo="biome"
 ver=$(curl -s https://api.github.com/repos/$org/$repo/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}')
 dl="https://github.com/$org/$repo/releases/download/$ver/bio-${ver#"v"}-x86_64-linux.tar.gz"
 echo "[info] getting bio from: $dl" && curl -L -o bio.gz $dl && tar -xf bio.gz 
 cp ./bio /usr/bin/bio && bio --version
 
-bio origin key generate mozillareality
-habCacheKeyPath="/hab/cache/keys"
-echo "habCacheKeyPath: $habCacheKeyPath"
-mkdir -p $habCacheKeyPath
-echo $BLDR_HAB_TOKEN > $habCacheKeyPath/mozillareality_hab
-echo $BLDR_RET_TOKEN > $habCacheKeyPath/mozillareality_ret
 export HAB_ORIGIN=mozillareality
-export HAB_ORIGIN_KEYS=mozillareality_hab
+
+mkdir -p /hab/cache/keys/
+mkdir -p ./hab/cache/keys/
+echo $BLDR_RET_PUB_B64 | base64 -d > /hab/cache/keys/mozillareality-20190117233449.pub
+echo $BLDR_RET_PUB_B64 | base64 -d > ./hab/cache/keys/mozillareality-20190117233449.pub
+echo $BLDR_HAB_PVT_B64 | base64 -d > /hab/cache/keys/mozillareality-20190117233449.sig.key
+echo $BLDR_HAB_PVT_B64 | base64 -d > /hab/cache/keys/mozillareality-20190117233449.sig.key
 
 echo "### build hab pkg"
 export HAB_AUTH_TOKEN=$BLDR_HAB_TOKEN
@@ -96,7 +95,7 @@ do_end() {
     return 0
 }
 EOF
-bio pkg build --cache-key-path $habCacheKeyPath -k mozillareality .
+bio pkg build -k mozillareality .
 
 ### upload
 echo "### upload hab pkg"
