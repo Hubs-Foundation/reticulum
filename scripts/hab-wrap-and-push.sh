41db677858f6b82f9acb8a94267e9a271b19b627
Bryan Enders <2033828+bryanenders@users.noreply.github.com>
2023-02-02 12:05:07 -0500

    Remove trailing whitespace

--- scripts/hab-wrap-and-push.sh
+++ scripts/hab-wrap-and-push.sh
@@ -10,7 +10,7 @@ apk add curl
 org="biome-sh";repo="biome"
 ver=$(curl -s https://api.github.com/repos/$org/$repo/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}')
 dl="https://github.com/$org/$repo/releases/download/$ver/bio-${ver#"v"}-x86_64-linux.tar.gz"
-echo "[info] getting bio from: $dl" && curl -L -o bio.gz $dl && tar -xf bio.gz 
+echo "[info] getting bio from: $dl" && curl -L -o bio.gz $dl && tar -xf bio.gz
 cp ./bio /usr/bin/bio && bio --version
 
 export HAB_ORIGIN=mozillareality
@@ -57,7 +57,7 @@ do_verify() {
 do_prepare() {
     export LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
     export MIX_ENV=prod
-    export RELEASE_VERSION="1.0.$(echo $pkg_prefix | cut -d '/' -f 7)" 
+    export RELEASE_VERSION="1.0.$(echo $pkg_prefix | cut -d '/' -f 7)"
 
     # Rebar3 will hate us otherwise because it looks for
     # /usr/bin/env when it does some of its compiling
