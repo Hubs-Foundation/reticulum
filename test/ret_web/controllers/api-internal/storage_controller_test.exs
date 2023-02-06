83d1b4fb8ce9fa8ea37bfb16753c6f83d39ccb2b
Robin Wilson <robin.kath.wilson@gmail.com>
2023-02-06 14:02:25 -0800

    reverted storage tests

--- test/ret_web/controllers/api-internal/storage_controller_test.exs
+++ test/ret_web/controllers/api-internal/storage_controller_test.exs
@@ -17,44 +17,32 @@ defmodule RetWeb.ApiInternal.V1.StorageControllerTest do
     end)
   end
 
-  test "hub stats endpoint responds with cached storage value", %{conn: conn} do
+  test "storage endpoint responds with cached storage value", %{conn: conn} do
     mock_storage_used(0)
-    resp = request_hub_stats(conn)
+    resp = request_storage(conn)
     assert resp["storage_mb"] === 0.0
 
     mock_storage_used(10)
-    resp = request_hub_stats(conn)
+    resp = request_storage(conn)
     assert resp["storage_mb"] === 10.0
   end
 
-
-  test "hub stats endpoint returns 401 without access key header", %{conn: conn} do
-    resp = get(conn, "/api-internal/v1/hub_stats")
+  test "storage endpoint errors without correct access key", %{conn: conn} do
+    resp = get(conn, "/api-internal/v1/storage")
     assert resp.status === 401
-  end
 
-  test "hub stats endpoint returns 401 with incorrect access key", %{conn: conn} do
     resp =
       conn
       |> put_req_header(@dashboard_access_header, "incorrect-access-key")
-      |> get("/api-internal/v1/hub_stats")
+      |> get("/api-internal/v1/storage")
 
     assert resp.status === 401
   end
 
-  test "hub stats endpoint errors with 503 status when storage usage is not available", %{conn: conn} do
+  test "storage endpoint errors when storage usage is not available", %{conn: conn} do
     mock_storage_used(nil)
-    resp = request_hub_stats(conn, expected_status: 503)
+    resp = request_storage(conn, expected_status: 503)
     assert resp["error"] === "storage_usage_unavailable"
-  end
-
-  test "hub stats endpoint returns correct ccu numbers for time range", %{conn: conn} do
-  end
-
-  test "hub stats endpoint returns 0 ccu numbers for time range if no data available", %{conn: conn} do
-  end
-
-  test "hub stats endpoint returns correct ccu numbers for time range when there is multiple ccu numbers in db", %{conn: conn} do
   end
 
   # The Ret.Storage module relies on a cached value to retrieve storage usage via Ret.StorageUsed.
@@ -65,14 +53,10 @@ defmodule RetWeb.ApiInternal.V1.StorageControllerTest do
   defp mock_storage_used(storage_used_mb),
     do: Cachex.put(:storage_used, :storage_used, storage_used_mb * 1024)
 
-  defp seed_ccu() do
-    # todo
-  end
-
-  defp request_hub_stats(conn, opts \\ [expected_status: 200]) do
+  defp request_storage(conn, opts \\ [expected_status: 200]) do
     conn
     |> put_req_header(@dashboard_access_header, @dashboard_access_key)
-    |> get("/api-internal/v1/hub_stats?")
+    |> get("/api-internal/v1/storage")
     |> json_response(opts[:expected_status])
   end
 end
