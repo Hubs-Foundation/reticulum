6ad589336148e67f49fc35a6104286e07a4b512b
Geng Tan <tanfarming@hotmail.com>
2023-02-17 12:31:23 -0500

    Update page_controller.ex

--- lib/ret_web/controllers/page_controller.ex
+++ lib/ret_web/controllers/page_controller.ex
@@ -428,6 +428,7 @@ defmodule RetWeb.PageController do
   end
 
   def render_hub_content(conn, nil, _) do
+    inspect(conn)
     conn |> send_resp(200, "Hub is not configured properly. (Homepage Room ID not valid.) Please contact your Hub administrator.")
   end
 
