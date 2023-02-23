ac110f0e65e40854a4b8922786fe96abace1470f
tanfarming <tanfarming@hotmail.com>
2023-02-23 10:36:35 -0500

    word

--- lib/ret_web/controllers/api/v1/media_controller.ex
+++ lib/ret_web/controllers/api/v1/media_controller.ex
@@ -114,6 +114,7 @@ defmodule RetWeb.Api.V1.MediaController do
   defp resolve_and_render(conn, url, version, quality \\ nil) do
     query = query_for(conn, url, version, quality)
     value = Cachex.fetch(:media_urls, query)
+    IO.inspect(value)
     maybe_do_telemetry(value)
     maybe_bump_ttl(value, query)
     render_resolved_media_or_error(conn, value)
