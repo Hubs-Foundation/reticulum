809c93b80f686d30fb3b5ef41630226957de8c7d
John Shaughnessy <john@johnshaughnessy.com>
2023-03-06 23:15:40 -0500

    Fix habitat resolution issue for erlang

--- scripts/hab-wrap-and-push.sh
+++ scripts/hab-wrap-and-push.sh
@@ -44,7 +44,7 @@ pkg_deps=(
 pkg_build_deps=(
     core/coreutils/8.30/20190115012313
     core/git/2.23.0
-    mozillareality/erlang/23.3.4.18
+    core/erlang/23.3.4.9
     mozillareality/elixir/1.14.3
 )
 pkg_exports=(
