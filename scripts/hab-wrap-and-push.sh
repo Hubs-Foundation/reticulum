d12646769c4535c4e882d0b3de8913bdf45d9e8c
Geng Tan <tanfarming@hotmail.com>
2023-04-06 18:18:25 -0400

    Update hab-wrap-and-push.sh

--- scripts/hab-wrap-and-push.sh
+++ scripts/hab-wrap-and-push.sh
@@ -37,12 +37,12 @@ pkg_maintainer="Mozilla Mixed Reality <mixreality@mozilla.com>"
 pkg_upstream_url="http://github.com/mozilla/reticulum"
 pkg_license=('MPL-2.0')
 pkg_deps=(
-    core/coreutils/8.30/20190115012313
-    core/bash/4.4.19/20190115012619
-    core/which/2.21/20190430084037
+    core/coreutils/8.32/20220311101609
+    core/bash/5.1/20220801055216
+    core/which/2.21/20220311145823
 )
 pkg_build_deps=(
-    core/coreutils/8.30/20190115012313
+    core/coreutils/8.32/20220311101609
     core/git/2.23.0
     mozillareality/erlang/23.3.4.18
     mozillareality/elixir/1.14.3
